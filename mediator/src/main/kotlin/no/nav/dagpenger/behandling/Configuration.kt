f50265b7e08b2c5292de83e3765e83bb05d7fec6
Geir André Lund <geir.andre.lund@nav.no>
2022-12-05 10:28:56 +0100

    Lyttet på journalforing.v1 topic
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/Configuration.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/Configuration.kt
@@ -14,6 +14,7 @@ internal object Configuration {
             "RAPID_APP_NAME" to appName,
             "KAFKA_CONSUMER_GROUP_ID" to "dp-behandling-v1",
             "KAFKA_RAPID_TOPIC" to "teamdagpenger.rapid.v1",
+            "KAFKA_EXTRA_TOPIC" to "teamdagpenger.journalforing.v1",
             "KAFKA_RESET_POLICY" to "latest",
         )
     )
