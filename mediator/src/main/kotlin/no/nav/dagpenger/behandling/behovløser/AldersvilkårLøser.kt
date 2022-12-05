b95a333353d3092d81ccb07cf1577017745911e2
Mona Kjeldsrud <Mona.Kjeldsrud.nav.no>
2022-12-05 13:53:50 +0100

    Refaktorert til konstant behov
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/behovløser/AldersvilkårLøser.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/behovløser/AldersvilkårLøser.kt
@@ -9,21 +9,23 @@ import no.nav.helse.rapids_rivers.River
 
 class AldersvilkårLøser(rapidsConnection: RapidsConnection) : River.PacketListener {
     private val logger = KotlinLogging.logger {}
+    private val behov = "Aldersbehov"
 
     init {
+
         River(rapidsConnection).apply {
             validate { it.demandValue("@event_name", "behov") }
             validate { it.requireKey("behandlingId", "ident", "@behovId") }
-            validate { it.demandAllOrAny("@behov", listOf("Aldersbehov")) }
+            validate { it.demandAllOrAny("@behov", listOf(behov)) }
             validate { it.forbid("@løsning") }
         }.register(this)
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         withLoggingContext("behandlingId" to packet["behandlingId"].asText()) {
-            packet["@løsning"] = mapOf("Aldersbehov" to true)
+            packet["@løsning"] = mapOf(behov to true)
             context.publish(packet.toJson())
-            logger.info { "Løser aldersbehov" }
+            logger.info { "Løser $behov" }
         }
     }
 }
