ce4e5b1cdfcd5ad7ae16d7aa5aeb5deef59f3122
Giao The Cung <giaothe@gmail.com>
2023-03-21 13:23:52 +0100

    Add an endpoint

---
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/Api2.kt
@@ -0,0 +1,26 @@
+package no.nav.dagpenger.behandling
+
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+
+fun Application.api() {
+
+    routing {
+        get("oppgaver") {
+            this.call.respondText(
+                contentType = ContentType.Application.Json, status = HttpStatusCode.OK
+            ) {
+                """
+                   {} 
+                """.trimIndent()
+            }
+        }
+    }
+
+}
+
