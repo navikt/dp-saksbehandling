b80955659c5ee23c003b34f705447a11bf38909d
Giao The Cung <giaothe@gmail.com>
2023-03-21 17:30:47 +0100

    ktlint on gradle kotlin files is just pain and no value

--- buildSrc/src/main/kotlin/dagpenger.spotless.gradle.kts
+++ buildSrc/src/main/kotlin/dagpenger.spotless.gradle.kts
@@ -6,12 +6,7 @@ plugins {
 
 configure<com.diffplug.gradle.spotless.SpotlessExtension> {
     kotlin {
-        ktfmt()
         ktlint("0.48.2")
-    }
-    kotlinGradle {
-        this.target("*.gradle.kts")
-        ktlint("0.48.2") // or ktfmt() or prettier()
     }
 }
 tasks.withType<KotlinCompile>().configureEach {
