d3cffc9aa3af6209d1ffa484b6ea7043e4ea8d03
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2023-03-30 10:07:39 +0200

    Gitt mer logiske navn til stegene i testen.
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>
    Co-authored-by: Giao The Cung <giaothe@gmail.com>

--- modell/src/test/kotlin/no/nav/dagpenger/behandling/dsl/BehandlingDSLTest.kt
+++ modell/src/test/kotlin/no/nav/dagpenger/behandling/dsl/BehandlingDSLTest.kt
@@ -10,37 +10,62 @@ class BehandlingDSLTest {
     @Test
     fun `dsl test`() {
         val behandling = behandling(Person("123")) {
-            val felles = steg {
-                fastsettelse<LocalDate>("felles")
+            val grandparentFelles = steg {
+                fastsettelse<LocalDate>("GrandparentFelles")
             }
 
             steg {
-                vilkår("første") {
-                    avhengerAv(felles)
+                vilkår("Grandparent2") {
+                    avhengerAv(grandparentFelles)
                 }
             }
 
             steg {
-                fastsettelse<Boolean>("blurp") {
-                    avhengerAvFastsettelse<Int>("blarp")
-                    avhengerAvFastsettelse<String>("burp") {
-                        avhengerAvFastsettelse<String>("blarpburp")
-                        avhengerAvVilkår("foobar")
-                        avhengerAv(felles)
+                fastsettelse<Boolean>("Grandparent3") {
+                    avhengerAvFastsettelse<Int>("Parent1")
+                    avhengerAvFastsettelse<String>("Parent2") {
+                        avhengerAvFastsettelse<String>("Child21")
+                        avhengerAvVilkår("Child22")
+                        avhengerAv(grandparentFelles)
                     }
                 }
             }
         }
 
-        behandling.steg.map { it.id } shouldBe setOf("felles", "første", "blurp")
+        behandling.steg.map { it.id } shouldBe setOf("GrandparentFelles", "Grandparent2", "Grandparent3")
 
         behandling.alleSteg().size shouldBe 7
-        behandling.nesteSteg().map { it.id } shouldBe setOf("felles", "blarp", "blarpburp", "foobar")
+        behandling.alleSteg().map { it.id } shouldBe setOf("GrandparentFelles", "Grandparent2", "Grandparent3", "Parent1", "Parent2", "Child21", "Child22")
+        behandling.nesteSteg().map { it.id } shouldBe setOf("GrandparentFelles", "Parent1", "Child21", "Child22")
         behandling.nesteSteg().size shouldBe 4
 
         // Besvare
-        val felles = behandling.nesteSteg().single { it.id == "felles" }
-        behandling.besvar(felles.uuid, LocalDate.now())
-        behandling.nesteSteg().map { it.id } shouldBe setOf("første", "blarp", "blarpburp", "foobar")
+        val grandparentFelles = behandling.nesteSteg().single { it.id == "GrandparentFelles" }
+        behandling.besvar(grandparentFelles.uuid, LocalDate.now())
+        behandling.nesteSteg().map { it.id } shouldBe setOf("Grandparent2", "Parent1", "Child21", "Child22")
+
+        val grandparent2 = behandling.nesteSteg().single { it.id == "Grandparent2" }
+        behandling.besvar(grandparent2.uuid, true)
+        behandling.nesteSteg().map { it.id } shouldBe setOf("Parent1", "Child21", "Child22")
+
+        val parent1 = behandling.nesteSteg().single { it.id == "Parent1" }
+        behandling.besvar(parent1.uuid, 3)
+        behandling.nesteSteg().map { it.id } shouldBe setOf("Child21", "Child22")
+
+        val child21 = behandling.nesteSteg().single { it.id == "Child21" }
+        behandling.besvar(child21.uuid, "Dette er et svare")
+        behandling.nesteSteg().map { it.id } shouldBe setOf("Child22")
+
+        val child22 = behandling.nesteSteg().single { it.id == "Child22" }
+        behandling.besvar(child22.uuid, true)
+        behandling.nesteSteg().map { it.id } shouldBe setOf("Parent2")
+
+        val parent2 = behandling.nesteSteg().single { it.id == "Parent2" }
+        behandling.besvar(parent2.uuid, "Dette er et svar også")
+        behandling.nesteSteg().map { it.id } shouldBe setOf("Grandparent3")
+
+        val grandparent3 = behandling.nesteSteg().single { it.id == "Grandparent3" }
+        behandling.besvar(grandparent3.uuid, true)
+        behandling.nesteSteg().map { it.id } shouldBe emptyList()
     }
 }
