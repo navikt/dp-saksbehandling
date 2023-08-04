f700a68da7693a7122f0d422125725aea581dec4
Geir André Lund <geir.andre.lund@nav.no>
2023-08-04 08:52:22 +0200

    Setter tidssone til OffsetDateTime på Sporing Fikser feil - java.time.DateTimeException: Invalid ID for ZoneOffset, invalid format: Europe/Oslo

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/dto/BegrunnelseDTO.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/dto/BegrunnelseDTO.kt
@@ -10,19 +10,20 @@ import no.nav.dagpenger.behandling.api.models.QuizBegrunnelseDTO
 import no.nav.dagpenger.behandling.api.models.SaksbehandlerDTO
 import no.nav.dagpenger.behandling.api.models.SaksbehandlersBegrunnelseDTO
 import no.nav.dagpenger.behandling.oppgave.Saksbehandler
-import java.time.ZoneOffset
+import java.time.ZoneId
 
+private val Europa_Oslo = ZoneId.of("Europe/Oslo")
 fun Sporing.toBegrunnelseDTO(): BegrunnelseDTO? {
     return when (this) {
         is QuizSporing -> QuizBegrunnelseDTO(
             kilde = KildeDTO.Quiz,
-            utført = this.utført.atOffset(ZoneOffset.of("Europe/Oslo")),
+            utført = this.utført.atZone(Europa_Oslo).toOffsetDateTime(),
             json = this.json,
         )
 
         is ManuellSporing -> SaksbehandlersBegrunnelseDTO(
             kilde = KildeDTO.Saksbehandler,
-            utført = this.utført.atOffset(ZoneOffset.of("Europe/Oslo")),
+            utført = this.utført.atZone(Europa_Oslo).toOffsetDateTime(),
             utførtAv = this.utførtAv.toSaksbehandlerDTO(),
             tekst = this.begrunnelse,
         )
