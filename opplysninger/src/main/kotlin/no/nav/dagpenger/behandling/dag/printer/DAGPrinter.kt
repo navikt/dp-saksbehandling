5630745e29fc68ccf45f74f4a947318a1a1c04cd
Geir André Lund <geir.andre.lund@nav.no>
2024-02-01 10:35:05 +0100

    FLyttet opplysnigner (aka regelmotor) til dp-vedtak
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>

--- opplysninger/src/main/kotlin/no/nav/dagpenger/behandling/dag/printer/DAGPrinter.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.dagpenger.behandling.dag.printer
-
-import no.nav.dagpenger.behandling.Opplysningstype
-
-interface DAGPrinter {
-    fun toPrint(root: Opplysningstype<Boolean>? = null): String
-}
