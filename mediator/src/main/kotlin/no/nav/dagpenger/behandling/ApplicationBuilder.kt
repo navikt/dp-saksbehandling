c86ef04994a5e63304bb7a39f62e03e8dd6f1668
Giao The Cung <giaothe@gmail.com>
2024-02-06 09:31:17 +0100

    wire up oppgaveapi
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/ApplicationBuilder.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/ApplicationBuilder.kt
@@ -1,17 +1,19 @@
 package no.nav.dagpenger.behandling
 
 import mu.KotlinLogging
+import no.nav.dagpenger.behandling.api.oppgaveApi
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
 
 internal class ApplicationBuilder(configuration: Map<String, String>) : RapidsConnection.StatusListener {
+    private val mediator =
+        Mediator()
     private val rapidsConnection: RapidsConnection =
         RapidApplication.Builder(RapidApplication.RapidApplicationConfig.fromEnv(configuration))
             .withKtorModule {
+                this.oppgaveApi(mediator)
             }.build()
 
-    private val mediator =
-        Mediator()
 
     init {
         rapidsConnection.register(this)
