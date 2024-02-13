ec87b8fff08fc7104b961a9a121670ed73d83304
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2024-02-13 11:02:03 +0100

    Mottak av behandling_opprettet hendelse
    Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>

---
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mottak/BehandlingOpprettetMottak.kt
@@ -0,0 +1,28 @@
+package no.nav.dagpenger.behandling.mottak
+
+import mu.KotlinLogging
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.RapidsConnection
+import no.nav.helse.rapids_rivers.River
+
+internal class BehandlingOpprettetMottak(rapidsConnection: RapidsConnection) : River.PacketListener {
+    companion object {
+        private val logger = KotlinLogging.logger {}
+        val rapidFilter: River.() -> Unit = {
+            validate { it.demandValue("@event_name", "behandling_opprettet") }
+            validate { it.requireKey("ident", "søknadId", "behandlingId") }
+        }
+    }
+
+    init {
+        River(rapidsConnection).apply(rapidFilter).register(this)
+    }
+
+    override fun onPacket(
+        packet: JsonMessage,
+        context: MessageContext,
+    ) {
+        println("Hei her er jeg!")
+    }
+}
