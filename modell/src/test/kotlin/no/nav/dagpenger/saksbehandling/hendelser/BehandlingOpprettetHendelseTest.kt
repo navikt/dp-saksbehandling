bb1d40f4ebeef870242c7b99a67d5df7f5f16ac2
Marius Eriksen <marius.eriksen@nav.no>
2024-02-15 12:31:35 +0100

    Utvid test og fjern ubrukt diagram
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

---
+++ modell/src/test/kotlin/no/nav/dagpenger/saksbehandling/hendelser/BehandlingOpprettetHendelseTest.kt
@@ -0,0 +1,27 @@
+package no.nav.dagpenger.saksbehandling.hendelser
+
+import io.kotest.matchers.shouldBe
+import no.nav.dagpenger.saksbehandling.Person
+import no.nav.dagpenger.saksbehandling.UUIDv7
+import org.junit.jupiter.api.Test
+
+class BehandlingOpprettetHendelseTest {
+    private val ident = "12345612345"
+    private val person = Person(ident)
+    private val behandlingId = UUIDv7.ny()
+
+    @Test
+    fun `BehandlingOpprettetHendelse skal føre til at person opprettes med en behandling`() {
+        val behandlingOpprettetHendelse =
+            BehandlingOpprettetHendelse(
+                søknadId = UUIDv7.ny(),
+                behandlingId = behandlingId,
+                ident = ident,
+            )
+        person.håndter(behandlingOpprettetHendelse)
+
+        person.ident shouldBe ident
+        person.behandlinger.size shouldBe 1
+        person.behandlinger.first().behandlingId shouldBe behandlingId
+    }
+}
