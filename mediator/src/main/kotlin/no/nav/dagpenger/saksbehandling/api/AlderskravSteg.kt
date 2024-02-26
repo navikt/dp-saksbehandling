9d94eb1b02840f7baa1fde47d8ce52487c546af8
Marius Eriksen <marius.eriksen@nav.no>
2024-02-26 11:18:20 +0100

    wip: Hent ut alle opplysninger fra behandling
    Co-authored-by: Giao The Cung <giaothe@gmail.com>
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/api/AlderskravSteg.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/api/AlderskravSteg.kt
@@ -1,6 +1,7 @@
 package no.nav.dagpenger.saksbehandling.api
 
 import no.nav.dagpenger.behandling.opplysninger.api.models.BehandlingDTO
+import no.nav.dagpenger.behandling.opplysninger.api.models.UtledningDTO
 import no.nav.dagpenger.saksbehandling.UUIDv7
 import no.nav.dagpenger.saksbehandling.api.models.OpplysningDTO
 import no.nav.dagpenger.saksbehandling.api.models.OpplysningTypeDTO
@@ -15,13 +16,13 @@ fun alderskravStegFra(behandlingDTO: BehandlingDTO?): StegDTO? {
                 uuid = UUIDv7.ny(),
                 stegNavn = "Under 67 år",
                 opplysninger =
-                    listOf(
-                        OpplysningDTO(
-                            opplysningNavn = "Under 67 år",
-                            opplysningType = OpplysningTypeDTO.Boolean,
-                            svar = SvarDTO(alderskravOpplysning.verdi),
-                        ),
-                    ) + opplysningsgrunnlagFor(alderskravOpplysning),
+                listOf(
+                    OpplysningDTO(
+                        opplysningNavn = "Under 67 år",
+                        opplysningType = OpplysningTypeDTO.Boolean,
+                        svar = SvarDTO(alderskravOpplysning.verdi),
+                    ),
+                ) + opplysningsgrunnlagFor(alderskravOpplysning),
             )
 
         else -> null
@@ -31,18 +32,28 @@ fun alderskravStegFra(behandlingDTO: BehandlingDTO?): StegDTO? {
 private fun alderskravOpplysningFra(behandling: BehandlingDTO?) =
     behandling?.opplysning?.findLast { it.opplysningstype == "Oppfyller kravet til alder" }
 
-private fun opplysningsgrunnlagFor(opplysning: no.nav.dagpenger.behandling.opplysninger.api.models.OpplysningDTO) =
-    opplysning.utledetAv?.opplysninger?.map {
+private fun tull(utledningDTO: UtledningDTO?, opplysninger: MutableList<OpplysningDTO>) {
+    if(utledningDTO == null) return
+    
+}
+
+private fun opplysningsgrunnlagFor(opplysning: no.nav.dagpenger.behandling.opplysninger.api.models.OpplysningDTO): List<OpplysningDTO> {
+    val opplysninger = mutableListOf<OpplysningDTO>()
+    opplysning.utledetAv!!.opplysninger.forEach {
+
+    }
+    return opplysning.utledetAv?.opplysninger?.map {
         OpplysningDTO(
             opplysningNavn = it.opplysningstype,
             opplysningType =
-                when (it.datatype) {
-                    "boolean" -> OpplysningTypeDTO.Boolean
-                    "string" -> OpplysningTypeDTO.String
-                    "double" -> OpplysningTypeDTO.Double
-                    "LocalDate" -> OpplysningTypeDTO.LocalDate
-                    else -> OpplysningTypeDTO.String
-                },
+            when (it.datatype) {
+                "boolean" -> OpplysningTypeDTO.Boolean
+                "string" -> OpplysningTypeDTO.String
+                "double" -> OpplysningTypeDTO.Double
+                "LocalDate" -> OpplysningTypeDTO.LocalDate
+                else -> OpplysningTypeDTO.String
+            },
             svar = SvarDTO(it.verdi),
         )
     } ?: emptyList()
+}
