3a040735823e18ea6891aff4a393f82e417a4410
Giao The Cung <giaothe@gmail.com>
2024-03-07 09:14:09 +0100

    Legge til plumbing kode for database
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

---
+++ mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/db/PostgresMigrationTest.kt
@@ -0,0 +1,16 @@
+package no.nav.dagpenger.saksbehandling.db
+
+import io.kotest.matchers.shouldBe
+import no.nav.dagpenger.saksbehandling.db.Postgres.withCleanDb
+import no.nav.dagpenger.saksbehandling.db.PostgresDataSourceBuilder.runMigration
+import org.junit.jupiter.api.Test
+
+class PostgresMigrationTest {
+    @Test
+    fun `Migration scripts are applied successfully`() {
+        withCleanDb {
+            val migrations = runMigration()
+            migrations shouldBe 0
+        }
+    }
+}
