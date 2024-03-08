9018c2a7b54d4ae22712dd708d6b074515c69eed
Marius Eriksen <marius.eriksen@nav.no>
2024-03-08 13:13:26 +0100

    Bruk BehandlingKlient interface i mediator
    Co-authored-by: Giao The Cung <giaothe@gmail.com>

--- mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/MediatorTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/MediatorTest.kt
@@ -22,7 +22,7 @@ class MediatorTest {
     private val testRapid = TestRapid()
     private val inMemoryRepository = InMemoryRepository()
     private val mediator =
-        Mediator(personRepository = inMemoryRepository, inMemoryRepository, behandlingHttpKlient = mockk())
+        Mediator(personRepository = inMemoryRepository, inMemoryRepository, behandlingKlient = mockk())
 
     init {
         BehandlingOpprettetMottak(testRapid, mediator)
