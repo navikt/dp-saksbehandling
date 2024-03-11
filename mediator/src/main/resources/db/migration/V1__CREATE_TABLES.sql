c530d504abb822ceca139b21ad22fd7fa2dc8f30
Marius Eriksen <marius.eriksen@nav.no>
2024-03-11 11:21:34 +0100

    Sett NOT NULL på flere kolonner
    Co-authored-by: Giao The Cung <giaothe@gmail.com>
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- mediator/src/main/resources/db/migration/V1__CREATE_TABLES.sql
+++ mediator/src/main/resources/db/migration/V1__CREATE_TABLES.sql
@@ -1,20 +1,20 @@
 CREATE TABLE IF NOT EXISTS person_v1
 (
     id    UUID PRIMARY KEY,
-    ident VARCHAR(11) UNIQUE
+    ident VARCHAR(11) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS behandling_v1
 (
     id        UUID PRIMARY KEY,
-    person_id UUID REFERENCES person_v1 (id),
+    person_id UUID                     NOT NULL REFERENCES person_v1 (id),
     opprettet TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS oppgave_v1
 (
     id            UUID PRIMARY KEY,
-    behandling_id UUID REFERENCES behandling_v1 (id),
+    behandling_id UUID                     NOT NULL REFERENCES behandling_v1 (id),
     tilstand      TEXT                     NOT NULL,
     opprettet     TIMESTAMP WITH TIME ZONE NOT NULL
 );
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS oppgave_v1
 CREATE TABLE IF NOT EXISTS emneknagg_v1
 (
     id         BIGSERIAL PRIMARY KEY,
-    oppgave_id UUID REFERENCES oppgave_v1 (id),
-    emneknagg  TEXT
+    oppgave_id UUID NOT NULL REFERENCES oppgave_v1 (id),
+    emneknagg  TEXT NOT NULL
 );
 CREATE INDEX IF NOT EXISTS emneknagg_oppgave_id_index ON emneknagg_v1 (oppgave_id);
