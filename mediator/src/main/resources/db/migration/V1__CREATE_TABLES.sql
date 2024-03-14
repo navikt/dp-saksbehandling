d675bdde4a4886857dfdf9447d52c6630b54c6fb
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2024-03-14 14:28:32 +0100

    Fjernet IF NOT EXIST på trigger, siden det ikke fungerte.
    Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>
    Co-authored-by: Giao The Cung <giaothe@gmail.com>

--- mediator/src/main/resources/db/migration/V1__CREATE_TABLES.sql
+++ mediator/src/main/resources/db/migration/V1__CREATE_TABLES.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS person_v1
     registrert_tidspunkt  TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     endret_tidspunkt      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-CREATE INDEX IF NOT EXISTS person_ident_index ON person_v1 (ident);
+CREATE INDEX   IF NOT EXISTS person_ident_index ON person_v1 (ident);
 CREATE TRIGGER oppdater_endret_tidspunkt
 BEFORE UPDATE ON person_v1
 FOR EACH ROW EXECUTE FUNCTION oppdater_endret_tidspunkt();
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS behandling_v1
     registrert_tidspunkt  TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     endret_tidspunkt      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-CREATE INDEX IF NOT EXISTS behandling_person_id_index ON behandling_v1 (person_id);
+CREATE INDEX   IF NOT EXISTS behandling_person_id_index ON behandling_v1 (person_id);
 CREATE TRIGGER oppdater_endret_tidspunkt
 BEFORE UPDATE ON behandling_v1
 FOR EACH ROW EXECUTE FUNCTION oppdater_endret_tidspunkt();
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS oppgave_v1
     registrert_tidspunkt  TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     endret_tidspunkt      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-CREATE INDEX IF NOT EXISTS oppgave_behandling_id_index ON oppgave_v1 (behandling_id);
+CREATE INDEX   IF NOT EXISTS oppgave_behandling_id_index ON oppgave_v1 (behandling_id);
 CREATE TRIGGER oppdater_endret_tidspunkt
 BEFORE UPDATE ON oppgave_v1
 FOR EACH ROW EXECUTE FUNCTION oppdater_endret_tidspunkt();
@@ -51,10 +51,10 @@ CREATE TABLE IF NOT EXISTS emneknagg_v1
     oppgave_id            UUID                        NOT NULL REFERENCES oppgave_v1 (id),
     emneknagg             TEXT                        NOT NULL,
     registrert_tidspunkt  TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    endret_tidspunkt      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+    endret_tidspunkt      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT emneknagg_oppgave_unique UNIQUE (oppgave_id, emneknagg)
 );
-CREATE INDEX IF NOT EXISTS emneknagg_oppgave_id_index ON emneknagg_v1 (oppgave_id);
-ALTER TABLE emneknagg_v1 ADD CONSTRAINT emneknagg_oppgave_unique UNIQUE (oppgave_id, emneknagg);
+CREATE INDEX   IF NOT EXISTS emneknagg_oppgave_id_index ON emneknagg_v1 (oppgave_id);
 CREATE TRIGGER oppdater_endret_tidspunkt
 BEFORE UPDATE ON emneknagg_v1
 FOR EACH ROW EXECUTE FUNCTION oppdater_endret_tidspunkt();
