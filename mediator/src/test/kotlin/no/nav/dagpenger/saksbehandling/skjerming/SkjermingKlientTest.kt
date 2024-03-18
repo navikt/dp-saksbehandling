b0e2ebb9dbdcf3799c5e6441f7c7e2bd31ea1a06
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2024-03-18 19:08:57 +0100

    WIP: Test av SkjermingKlient

--- mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/skjerming/SkjermingKlientTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/skjerming/SkjermingKlientTest.kt
@@ -1,10 +1,33 @@
 package no.nav.dagpenger.saksbehandling.skjerming
 
+import io.kotest.matchers.shouldBe
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
+import io.ktor.http.headersOf
+import kotlinx.coroutines.runBlocking
+import no.nav.dagpenger.saksbehandling.createHttpClient
 import org.junit.jupiter.api.Test
 
 class SkjermingKlientTest {
+    private val testTokenProvider: () -> String = { -> "token" }
+    private val baseUrl = "http://baseUrl"
 
     @Test
     fun `Skal returnere 200 OK`() {
+        val mockEngine =
+            MockEngine { request ->
+                respond("true", headers = headersOf("Content-Type", "application/json"))
+            }
+        val skjermingHttpKlient =
+            SkjermingHttpKlient(
+                skjermingApiUrl = baseUrl,
+                tokenProvider = testTokenProvider,
+                httpClient = createHttpClient(engine = mockEngine),
+            )
+        val skjermingResultat: Result<Boolean> =
+            runBlocking {
+                skjermingHttpKlient.egenAnsatt("12345612345")
+            }
+        skjermingResultat shouldBe Result.success(true)
     }
 }
