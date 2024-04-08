990634c3ce921d47b70496f01c897475f55c7582
Giao The Cung <giaothe@gmail.com>
2024-04-08 14:51:28 +0200

    Logger litt mer
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>
    Co-authored-by: MariusEriksen <Marius.Eriksen@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/HttpClient.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/HttpClient.kt
@@ -6,8 +6,13 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.jackson.jackson
+import mu.KotlinLogging
 
+private val sikkerLogg = KotlinLogging.logger("tjenestekall")
 fun createHttpClient(engine: HttpClientEngine) =
     HttpClient(engine) {
         expectSuccess = true
@@ -17,6 +22,15 @@ fun createHttpClient(engine: HttpClientEngine) =
                 registerModule(JavaTimeModule())
                 disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            }
+        }
+
+        install(Logging) {
+            level = LogLevel.ALL
+            logger = object : Logger {
+                override fun log(message: String) {
+                    sikkerLogg.info { message }
+                }
             }
         }
     }
