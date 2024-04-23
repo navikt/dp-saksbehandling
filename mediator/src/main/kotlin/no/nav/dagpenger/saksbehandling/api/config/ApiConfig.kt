53ad8952588e808f874c6fcab549bd79d6bae6ae
Marius Eriksen <marius.eriksen@nav.no>
2024-04-23 13:43:42 +0200

    Sikkerlogg uhåndterte feil som fører til InternalServerError
    Co-authored-by: Giao The Cung <giaothe@gmail.com>
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/api/config/ApiConfig.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/api/config/ApiConfig.kt
@@ -10,14 +10,26 @@ import io.ktor.server.auth.Authentication
 import io.ktor.server.plugins.callloging.CallLogging
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.plugins.statuspages.StatusPages
+import io.ktor.server.request.document
 import io.ktor.server.response.respond
+import mu.KotlinLogging
 import no.nav.dagpenger.saksbehandling.api.config.auth.jwt
 import no.nav.dagpenger.saksbehandling.db.DataNotFoundException
 import java.time.format.DateTimeParseException
 
+private val logger = KotlinLogging.logger { }
+private val sikkerLogger = KotlinLogging.logger("tjenestekall")
+
 fun Application.apiConfig() {
     install(CallLogging) {
         disableDefaultColors()
+        filter { call ->
+            !setOf(
+                "isalive",
+                "isready",
+                "metrics",
+            ).contains(call.request.document())
+        }
     }
 
     install(ContentNegotiation) {
@@ -33,10 +45,13 @@ fun Application.apiConfig() {
     install(StatusPages) {
         exception<Throwable> { call, cause ->
             when (cause) {
-                is DataNotFoundException -> call.respond(HttpStatusCode.NotFound)
+                is DataNotFoundException -> call.respond(HttpStatusCode.NotFound) { cause.message.toString() }
                 is IllegalArgumentException -> call.respond(HttpStatusCode.BadRequest)
-                is DateTimeParseException -> call.respond(HttpStatusCode.BadRequest, cause.message.toString())
-                else -> call.respond(HttpStatusCode.InternalServerError)
+                is DateTimeParseException -> call.respond(HttpStatusCode.BadRequest) { cause.message.toString() }
+                else -> {
+                    sikkerLogger.error(cause) { "Uhåndtert feil: ${cause.message}" }
+                    call.respond(HttpStatusCode.InternalServerError)
+                }
             }
         }
     }
