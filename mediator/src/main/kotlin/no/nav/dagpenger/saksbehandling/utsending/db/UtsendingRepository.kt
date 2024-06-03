596a2d36d8919ce5a3942e06e902d1d7bbc966b7
Marius Eriksen <marius.eriksen@nav.no>
2024-06-03 13:35:05 +0200

    Språkrådet.
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/utsending/db/UtsendingRepository.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/utsending/db/UtsendingRepository.kt
@@ -3,10 +3,8 @@ package no.nav.dagpenger.saksbehandling.utsending.db
 import no.nav.dagpenger.saksbehandling.utsending.Utsending
 import java.util.UUID
 
-
 interface UtsendingRepository {
-
     fun lagre(utsending: Utsending)
-    fun hent(oppgaveId: UUID): Utsending
 
+    fun hent(oppgaveId: UUID): Utsending
 }
