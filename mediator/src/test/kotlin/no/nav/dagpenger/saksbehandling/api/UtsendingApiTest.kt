3be752032c4b8f510e55c96927d7d0b3634dac89
Marius Eriksen <marius.eriksen@nav.no>
2024-06-04 14:17:34 +0200

    Test av happy path for UtsendingApi
    Co-authored-by: Giao The Cung <giaothe@gmail.com>
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/api/UtsendingApiTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/api/UtsendingApiTest.kt
@@ -4,13 +4,20 @@ import io.kotest.matchers.shouldBe
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.header
 import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
 import io.ktor.server.testing.ApplicationTestBuilder
 import io.ktor.server.testing.testApplication
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import no.nav.dagpenger.saksbehandling.UUIDv7
 import no.nav.dagpenger.saksbehandling.UtsendingMediator
+import no.nav.dagpenger.saksbehandling.utsending.hendelser.VedtaksbrevHendelse
 import org.junit.jupiter.api.Test
 
 class UtsendingApiTest {
@@ -23,6 +30,25 @@ class UtsendingApiTest {
                 "NAVident" to testNAVIdent,
             ),
         )
+
+    @Test
+    fun `Sending av brev går fint`() {
+        val oppgaveId = UUIDv7.ny()
+        val brev = "Html ja"
+        val utsendingMediatorMock =
+            mockk<UtsendingMediator>().also {
+                every { it.mottaBrev(VedtaksbrevHendelse(oppgaveId, brev)) } just Runs
+            }
+        withUtsendingApi(utsendingMediatorMock) {
+            val response =
+                client.post("/utsending/$oppgaveId/send-brev") {
+                    autentisert(token = gyldigToken)
+                    setBody(brev)
+                    contentType(ContentType.Text.Html)
+                }
+            response.status shouldBe HttpStatusCode.Accepted
+        }
+    }
 
     @Test
     fun `Skal avvise kall uten autoriserte AD grupper`() {
