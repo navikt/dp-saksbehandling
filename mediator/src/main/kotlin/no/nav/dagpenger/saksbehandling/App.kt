9302dacbeda96ea095ef37eb6a116daa50bb72c9
Giao The Cung <giaothe@gmail.com>
2024-07-02 16:15:01 +0200

    Separere appen i to coroutine scope

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/App.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/App.kt
@@ -1,5 +1,25 @@
 package no.nav.dagpenger.saksbehandling
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger { }
+
 fun main() {
-    ApplicationBuilder(Configuration.config).start()
+    runBlocking {
+        this.launch(Dispatchers.IO) {
+            logger.info { "Starter nye streams greier" }
+            while (true) {
+                delay(50000)
+                logger.info { "Nye streams greier leverer" }
+            }
+        }
+        this.launch {
+            logger.info { "ApplicationBuilder starter" }
+            ApplicationBuilder(Configuration.config).start()
+        }
+    }
 }
