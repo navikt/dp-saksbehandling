fd0db23c4d3a78fbbe68852b347566aacb5df4e5
Giao The Cung <giaothe@gmail.com>
2024-07-03 12:35:38 +0200

    Revert coroutine launch

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/App.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/App.kt
@@ -2,8 +2,6 @@ package no.nav.dagpenger.saksbehandling
 
 import mu.KotlinLogging
 
-private val logger = KotlinLogging.logger { }
-
 fun main() {
     ApplicationBuilder(Configuration.config).start()
 }
