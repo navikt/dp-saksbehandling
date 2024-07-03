28c0dab3c7d3aca335eec1f874e2ac02b8db172c
Giao The Cung <giaothe@gmail.com>
2024-07-03 12:35:11 +0200

    revert coroutine launch

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/App.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/App.kt
@@ -1,25 +1,9 @@
 package no.nav.dagpenger.saksbehandling
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger { }
 
 fun main() {
-    runBlocking {
-        this.launch(Dispatchers.IO) {
-            logger.info { "Starter nye streams greier" }
-            while (true) {
-                delay(50000)
-                logger.info { "Nye streams greier leverer" }
-            }
-        }
-        this.launch {
-            logger.info { "ApplicationBuilder starter" }
-            ApplicationBuilder(Configuration.config).start()
-        }
-    }
+    ApplicationBuilder(Configuration.config).start()
 }
