d666e70956c26b2670e01d8eea089a35cc8b72d4
Marius Eriksen <marius.eriksen@nav.no>
2024-08-02 11:25:06 +0200

    Legg til avro schema for Personhendelse, samt gradle plugin for avro
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- streams-consumer/build.gradle.kts
+++ streams-consumer/build.gradle.kts
@@ -1,13 +1,35 @@
 plugins {
     id("common")
+    id("com.github.davidmc24.gradle.plugin.avro") version "1.4.0"
+}
+
+repositories {
+    maven {
+        url = uri("https://packages.confluent.io/maven")
+    }
+}
+
+avro {
+    fieldVisibility.set("PRIVATE")
+    stringType.set("String")
+    outputCharacterEncoding.set("UTF-8")
 }
 
 dependencies {
     api("org.apache.kafka:kafka-streams:3.8.0")
+    implementation("org.apache.avro:avro:1.11.3")
     implementation(libs.kotlin.logging)
     implementation(libs.ktor.server.cio)
     implementation(libs.konfig)
 
     testImplementation("org.apache.kafka:kafka-streams-test-utils:3.8.0")
     testImplementation(libs.bundles.kotest.assertions)
+}
+
+tasks.named("runKtlintFormatOverTestSourceSet") {
+    dependsOn(tasks.named("generateTestAvroJava"))
+}
+
+tasks.named("runKtlintCheckOverTestSourceSet") {
+    dependsOn(tasks.named("generateTestAvroJava"))
 }
