410fd9ee7923db540d9ef1281493d21332fab885
Marius Eriksen <marius.eriksen@nav.no>
2024-08-14 13:15:17 +0200

    Deploy main til dev, og rydd i unødvendig logging
    Co-authored-by: Giao The Cung <giaothe@gmail.com>
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- streams-consumer/src/main/kotlin/no/nav/dagpenger/saksbehandling/streams/skjerming/SkjermetPersonStatusTopology.kt
+++ streams-consumer/src/main/kotlin/no/nav/dagpenger/saksbehandling/streams/skjerming/SkjermetPersonStatusTopology.kt
@@ -1,12 +1,8 @@
 package no.nav.dagpenger.saksbehandling.streams.skjerming
 
-import mu.KotlinLogging
 import no.nav.dagpenger.saksbehandling.streams.kafka.stringSerde
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.kstream.Consumed
-
-private val logger = KotlinLogging.logger {}
-private val sikkerlogg = KotlinLogging.logger("tjenestekall")
 
 fun StreamsBuilder.skjermetPersonStatus(
     topic: String,
@@ -15,9 +11,5 @@ fun StreamsBuilder.skjermetPersonStatus(
     this
         .stream(topic, Consumed.with(stringSerde, stringSerde))
         .mapValues { _, value -> value.toBoolean() }
-        .peek(loggPakke)
         .foreach(håndter)
 
-private val loggPakke: (String, Boolean) -> Unit = { fnr, status ->
-    logger.info { "Mottok melding om skjermet person" }
-}
