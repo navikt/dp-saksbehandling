b7765d42e7cc8504f16befb634a9e4a12cf9bbdc
Giao The Cung <giaothe@gmail.com>
2024-08-14 14:29:11 +0200

    Revert "Bruke httpklient uten prometheus"
    This reverts commit 25670dbcc0f964d5892bd831fc2f9abf1f300c15.
    
    Co-authored-by: MariusEriksen <Marius.Eriksen@nav.no>
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/AdressebeskyttelseMigrering.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/AdressebeskyttelseMigrering.kt
@@ -1,46 +1,15 @@
 package no.nav.dagpenger.saksbehandling
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.HttpRequestRetry
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.jackson.jackson
 import kotliquery.queryOf
 import kotliquery.sessionOf
 import mu.KotlinLogging
 import no.nav.dagpenger.saksbehandling.db.PostgresDataSourceBuilder
-import no.nav.dagpenger.saksbehandling.db.oppgave.PostgresOppgaveRepository
 import no.nav.dagpenger.saksbehandling.pdl.PDLHttpKlient
 
-private fun httpKlient(engine: HttpClientEngine = CIO.create {}) =
-    HttpClient(engine) {
-        expectSuccess = true
-        install(ContentNegotiation) {
-            jackson {
-                configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                setSerializationInclusion(JsonInclude.Include.NON_NULL)
-                registerModules(JavaTimeModule())
-            }
-        }
-
-        install(HttpRequestRetry) {
-            retryOnException(maxRetries = 5)
-            this.constantDelay(
-                millis = 100,
-                randomizationMs = 0,
-            )
-        }
-    }
-
-private val pdlKlient =
+internal val pdlKlient =
     PDLHttpKlient(
         url = Configuration.pdlUrl,
         tokenSupplier = Configuration.pdlTokenProvider,
-        httpClient = httpKlient(),
     )
 
 private val logger = KotlinLogging.logger { }
@@ -59,11 +28,26 @@ suspend fun adressebeskyttelseMigrering() {
             logger.info { "Hentet ${it.size} unike identer fra databasen" }
         }
 
-    val repo = PostgresOppgaveRepository(dataSource = PostgresDataSourceBuilder.dataSource)
-
     alleIdenter.forEach { ident ->
         val adresseBeskyttelse = pdlKlient.person(ident).getOrThrow().adresseBeskyttelseGradering
-        repo.oppdaterAdressebeskyttetStatus(ident, adresseBeskyttelse)
+        sessionOf(PostgresDataSourceBuilder.dataSource).use { session ->
+            session.run(
+                queryOf(
+                    //language=PostgreSQL
+                    statement =
+                        """
+                        UPDATE person_v1
+                        SET    adressebeskyttelse_gradering = :adresseBeskyttelseGradering
+                        WHERE  ident = :fnr
+                        """.trimIndent(),
+                    paramMap =
+                        mapOf(
+                            "fnr" to ident,
+                            "adresseBeskyttelseGradering" to adresseBeskyttelse.name,
+                        ),
+                ).asUpdate,
+            )
+        }
     }
     logger.info { "Migrering av adressebeskyttelse ferdig" }
 }
