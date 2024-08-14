cce5c19c8d96362c01fffe955414884d900ddcdf
Giao The Cung <giaothe@gmail.com>
2024-08-14 14:29:42 +0200

    Revert "logg feil ved migrering"
    This reverts commit ac965a118f4397876e94649844802196fa9ca018.
    
    Co-authored-by: MariusEriksen <Marius.Eriksen@nav.no>
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/App.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/App.kt
@@ -1,16 +1,12 @@
 package no.nav.dagpenger.saksbehandling
 
 import kotlinx.coroutines.runBlocking
-import mu.KotlinLogging
 import kotlin.system.exitProcess
-
-private val logger = KotlinLogging.logger {}
 
 fun main() {
     try {
         runBlocking { adressebeskyttelseMigrering() }
     } catch (e: Exception) {
-        logger.error { "Feil ved migrering av adressebeskyttelse: ${e.message}" }
         exitProcess(-1)
     }
     ApplicationBuilder(Configuration.config).start()
