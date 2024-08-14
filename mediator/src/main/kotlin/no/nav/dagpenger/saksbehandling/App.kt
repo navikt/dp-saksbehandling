a93bfc59a12b87c31d7320c54f1cf8898a2ec189
Giao The Cung <giaothe@gmail.com>
2024-08-14 14:30:00 +0200

    Revert "Lag engangsjobb for å adressebeskyttelse migrere alle personene i databasen"
    This reverts commit ed80d28afafa7201cf66891b877e2d5805a8e4d5.
    
    Co-authored-by: MariusEriksen <Marius.Eriksen@nav.no>
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/App.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/App.kt
@@ -1,13 +1,5 @@
 package no.nav.dagpenger.saksbehandling
 
-import kotlinx.coroutines.runBlocking
-import kotlin.system.exitProcess
-
 fun main() {
-    try {
-        runBlocking { adressebeskyttelseMigrering() }
-    } catch (e: Exception) {
-        exitProcess(-1)
-    }
     ApplicationBuilder(Configuration.config).start()
 }
