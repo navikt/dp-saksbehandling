4f9f9ec19b8072a1751aa809c638fd13be8fabcf
Giao The Cung <giaothe@gmail.com>
2024-08-14 12:08:50 +0200

    Ktlinting

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/api/tilgangskontroll/OppgaveTilgangskontroll.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/api/tilgangskontroll/OppgaveTilgangskontroll.kt
@@ -51,7 +51,8 @@ suspend fun PipelineContext<*, ApplicationCall>.oppgaveTilgangskontroll(tilgangs
 
             else -> {
                 logger.info {
-                    "Saksbehandler ${saksbehandler.navIdent} har IKKE tilgang til oppgave med id $oppgaveId. Tilganger: ${saksbehandler.grupper}"
+                    "Saksbehandler ${saksbehandler.navIdent} har IKKE tilgang til oppgave med id $oppgaveId." +
+                        " Tilganger: ${saksbehandler.grupper}"
                 }
                 throw IngenTilgangTilOppgaveException(feilendeValidering.feilmelding(oppgaveId, saksbehandler))
             }
