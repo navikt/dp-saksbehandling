ed80d28afafa7201cf66891b877e2d5805a8e4d5
Marius Eriksen <marius.eriksen@nav.no>
2024-08-14 13:46:00 +0200

    Lag engangsjobb for å adressebeskyttelse migrere alle personene i databasen
    Co-authored-by: Giao The Cung <giaothe@gmail.com>
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/App.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/App.kt
@@ -1,5 +1,13 @@
 package no.nav.dagpenger.saksbehandling
 
+import kotlinx.coroutines.runBlocking
+import kotlin.system.exitProcess
+
 fun main() {
+    try {
+        runBlocking { adressebeskyttelseMigrering() }
+    } catch (e: Exception) {
+        exitProcess(-1)
+    }
     ApplicationBuilder(Configuration.config).start()
 }
