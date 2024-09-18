c7cdb288334f9091a1525b86a2c343a2bef43ead
Giao The Cung <giaothe@gmail.com>
2024-09-18 13:01:11 +0200

    Legge til test saksbhehandlienr
    Co-authored-by: MariusEriksen <Marius.Eriksen@nav.no>
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/api/tilgangskontroll/PersonligTilgangsKontroll.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/api/tilgangskontroll/PersonligTilgangsKontroll.kt
@@ -2,21 +2,21 @@ package no.nav.dagpenger.saksbehandling.api.tilgangskontroll
 
 import java.util.UUID
 
-object KathrineTilgangskontroll : OppgaveTilgangskontroll {
+object PersonligTilgangsKontroll : OppgaveTilgangskontroll {
     override fun harTilgang(
         oppgaveId: UUID,
         saksbehandler: Saksbehandler,
-    ): Boolean = saksbehandler.navIdent.uppercase() == "G151133"
+    ): Boolean = saksbehandler.navIdent.uppercase() in listOf("G151133","Z994251")
 
     override fun feilmelding(
         oppgaveId: UUID,
         saksbehandler: Saksbehandler,
     ): String {
-        return "Saksbehandler er ikke Kathrine"
+        return "Saksbehandler ${saksbehandler.navIdent} har ikke tilgang til oppgave med id $oppgaveId"
     }
 
     override fun feilType(
         oppgaveId: UUID,
         saksbehandler: Saksbehandler,
-    ): String = "KathrineTilgangskontroll"
+    ): String = "PersonligTilgangsKontroll"
 }
