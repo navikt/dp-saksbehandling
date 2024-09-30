f90ccf7287f7dc34270a9c42a48ec0a91e23389d
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2024-09-30 15:55:20 +0200

    BehandlingAvbruttHendelse er en Hendelse der aktør er dp-behandling

--- modell/src/main/kotlin/no/nav/dagpenger/saksbehandling/hendelser/BehandlingAvbruttHendelse.kt
+++ modell/src/main/kotlin/no/nav/dagpenger/saksbehandling/hendelser/BehandlingAvbruttHendelse.kt
@@ -1,9 +1,11 @@
 package no.nav.dagpenger.saksbehandling.hendelser
 
+import no.nav.dagpenger.saksbehandling.Aktør
 import java.util.UUID
 
 data class BehandlingAvbruttHendelse(
     val behandlingId: UUID,
     val søknadId: UUID,
     val ident: String,
-)
+    private val aktør: Aktør = Aktør.System.dpBehandling,
+) : Hendelse(aktør)
