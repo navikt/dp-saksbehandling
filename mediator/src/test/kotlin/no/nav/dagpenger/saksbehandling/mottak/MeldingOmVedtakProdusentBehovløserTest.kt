5426bd97abc93b0c4d6e93c03a02a4ca063acae0
Giao The Cung <giaothe@gmail.com>
2024-10-23 17:39:11 +0200

    Utvide test
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/mottak/MeldingOmVedtakProdusentBehovløserTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/mottak/MeldingOmVedtakProdusentBehovløserTest.kt
@@ -5,52 +5,69 @@ import io.kotest.assertions.json.shouldEqualSpecifiedJsonIgnoringOrder
 import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
 import io.mockk.mockk
-import io.mockk.verify
 import no.nav.dagpenger.saksbehandling.UUIDv7
 import no.nav.dagpenger.saksbehandling.utsending.UtsendingMediator
 import org.junit.jupiter.api.Test
+import java.util.UUID
 
 class MeldingOmVedtakProdusentBehovløserTest {
     private val rapid = TestRapid()
 
     @Test
-    fun `skal løse behov`() {
-        val behandlingId = UUIDv7.ny()
-        val behandlingIdString = behandlingId.toString()
+    fun `skal løse behov dersom filter matcher`() {
+        val behandlingMedUtsending = UUIDv7.ny()
+        val behandlingUtenUtsending = UUIDv7.ny()
 
         val utsendingMediator =
             mockk<UtsendingMediator>().also {
-                coEvery { it.utsendingFinnesForBehandling(behandlingId) } returns true
+                coEvery { it.utsendingFinnesForBehandling(behandlingMedUtsending) } returns true
+                coEvery { it.utsendingFinnesForBehandling(behandlingUtenUtsending) } returns false
             }
 
         MeldingOmVedtakProdusentBehovløser(rapid, utsendingMediator)
-        rapid.sendTestMessage(
+        rapid.sendBehov(behandlingMedUtsending)
+        rapid.sendBehov(behandlingUtenUtsending)
+
+        rapid.inspektør.size shouldBe 2
+
+        rapid.inspektør.message(0).toString() shouldEqualSpecifiedJsonIgnoringOrder
             """
             {
               "@event_name" : "behov",
               "@behov" : [ "MeldingOmVedtakProdusent" ],
-              "behandlingId" : "$behandlingIdString",
+              "behandlingId" : "$behandlingMedUtsending",
+              "ident" : "11109233444",
+              "@løsning" : {
+                  "MeldingOmVedtakProdusent" : "Dagpenger"
+              }
+            }
+            """.trimIndent()
+
+        rapid.inspektør.message(1).toString() shouldEqualSpecifiedJsonIgnoringOrder
+            """
+            {
+              "@event_name" : "behov",
+              "@behov" : [ "MeldingOmVedtakProdusent" ],
+              "behandlingId" : "$behandlingUtenUtsending",
+              "ident" : "11109233444",
+              "@løsning" : {
+                  "MeldingOmVedtakProdusent" : "Arena"
+              }
+            }
+            """.trimIndent()
+    }
+
+    private fun TestRapid.sendBehov(behandlingId: UUID) {
+        this.sendTestMessage(
+            """
+            {
+              "@event_name" : "behov",
+              "@behov" : [ "MeldingOmVedtakProdusent" ],
+              "behandlingId" : "$behandlingId",
               "ident" : "11109233444"
             }
             
             """.trimIndent(),
         )
-
-        rapid.inspektør.size shouldBe 1
-        rapid.inspektør.message(0).toString() shouldEqualSpecifiedJsonIgnoringOrder
-            """
-            {
-                      "@event_name" : "behov",
-                      "@behov" : [ "MeldingOmVedtakProdusent" ],
-                      "behandlingId" : "$behandlingIdString",
-                      "ident" : "11109233444",
-                      "@løsning" : {
-                          "MeldingOmVedtakProdusent" : "Dagpenger"
-                      }
-                    }
-            """.trimIndent()
-        verify(exactly = 1) {
-            utsendingMediator.utsendingFinnesForBehandling(behandlingId)
-        }
     }
 }
