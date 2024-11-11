7e682580d513f3acba2e3ef63432de1be35b1f5f
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2024-11-11 14:11:44 +0100

    Lagt til failure-test og ryddet litt
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/behandling/BehandlingHttpKlientKreverTotrinnskontrollTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/behandling/BehandlingHttpKlientKreverTotrinnskontrollTest.kt
@@ -3,8 +3,10 @@ package no.nav.dagpenger.saksbehandling.behandling
 import io.kotest.matchers.shouldBe
 import io.ktor.client.engine.mock.MockEngine
 import io.ktor.client.engine.mock.respond
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.headersOf
 import kotlinx.coroutines.runBlocking
+import no.nav.dagpenger.saksbehandling.UUIDv7
 import no.nav.dagpenger.saksbehandling.behandling.BehandlingHttpKlient.Companion.lagBehandlingHttpKlient
 import org.junit.jupiter.api.Test
 import java.util.UUID
@@ -22,8 +24,13 @@ class BehandlingHttpKlientKreverTotrinnskontrollTest {
             MockEngine { request ->
                 request.headers["Authorization"] shouldBe "Bearer tulleToken"
                 request.url.toString() shouldBe "$dpBehandlingApiUrl/$behandlingId"
-                respond(responseJson, headers = headersOf("Content-Type", "application/json"))
+                respond(
+                    content = responseJson,
+                    headers = headersOf("Content-Type", "application/json"),
+                    status = HttpStatusCode.NoContent,
+                )
             }
+
         val klient =
             BehandlingHttpKlient(
                 dpBehandlingApiUrl = dpBehandlingApiUrl,
@@ -47,9 +54,6 @@ class BehandlingHttpKlientKreverTotrinnskontrollTest {
     @Test
     fun `Test av request og parsing av response av behandling som ikke krever totrinnskontroll`() {
         val resourseRetriever = object {}.javaClass
-        val saksbehandlerToken = "token"
-        val behandlingId = UUID.fromString("019145eb-6fbb-769f-b1b1-d2450b383a98")
-        val dpBehandlingApiUrl = "https://dp-behandling.intern.dev.nav.no/behandling"
         val responseJson = resourseRetriever.getResource("/behandlingKreverIkkeTotrinnskontroll.json")!!.readText()
         val mockEngine =
             MockEngine { request ->
@@ -74,6 +78,38 @@ class BehandlingHttpKlientKreverTotrinnskontrollTest {
                 .let { kreverTotrinnskontroll ->
                     kreverTotrinnskontroll shouldBe false
                 }
+        }
+    }
+
+    @Test
+    fun `Test av at sjekk om krever totrinnskontroll feiler`() {
+        val behandlingIdSomFeiler = UUIDv7.ny()
+        val mockEngine =
+            MockEngine { request ->
+                request.headers["Authorization"] shouldBe "Bearer tulleToken"
+                request.url.toString() shouldBe "$dpBehandlingApiUrl/$behandlingIdSomFeiler"
+                respond(
+                    content = "Error",
+                    status = HttpStatusCode.InternalServerError,
+                )
+            }
+
+        val klient =
+            BehandlingHttpKlient(
+                dpBehandlingApiUrl = dpBehandlingApiUrl,
+                tokenProvider = { "tulleToken" },
+                httpClient =
+                    lagBehandlingHttpKlient(
+                        engine = mockEngine,
+                        metricsBaseName = "kubba",
+                    ),
+            )
+
+        runBlocking {
+            klient.kreverTotrinnskontroll(
+                behandlingId = behandlingIdSomFeiler,
+                saksbehandlerToken = saksbehandlerToken,
+            ).isFailure shouldBe true
         }
     }
 }
