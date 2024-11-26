80ce3a4efe0d77dbe7380e1442c60de42c0c1c52
Giao The Cung <giaothe@gmail.com>
2024-11-26 12:39:14 +0100

    Fjerne unødvendig logging

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/api/RelevanteJournalpostIdOppslag.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/api/RelevanteJournalpostIdOppslag.kt
@@ -3,7 +3,6 @@ package no.nav.dagpenger.saksbehandling.api
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import mu.KotlinLogging
-import mu.withLoggingContext
 import no.nav.dagpenger.saksbehandling.Behandling
 import no.nav.dagpenger.saksbehandling.Oppgave
 import no.nav.dagpenger.saksbehandling.hendelser.SøknadsbehandlingOpprettetHendelse
@@ -20,17 +19,7 @@ internal class RelevanteJournalpostIdOppslag(
         return coroutineScope {
             val journalpostIderSøknad = async { journalpostIdClient.hentJournalPostIder(oppgave.behandling) }
             val journalpostMeldingOmVedtak =
-                async { utsendingRepository.finnUtsendingFor(oppgave.oppgaveId)?.journalpostId() }.also {
-                    withLoggingContext(
-                        "oppgaveId" to oppgave.oppgaveId.toString(),
-                        "behandlingId" to oppgave.behandling.behandlingId.toString(),
-                    ) {
-                        sikkerlogg.debug {
-                            "For oppgave med tilstand ${oppgave.tilstand().type}, " +
-                                "hentes journalpostId for melding om vedtak: $it"
-                        }
-                    }
-                }
+                async { utsendingRepository.finnUtsendingFor(oppgave.oppgaveId)?.journalpostId() }
             (journalpostIderSøknad.await() + journalpostMeldingOmVedtak.await()).filterNotNull().toSet()
         }
     }
