0c550d4558590fc26bfa2a55380e09c3eafb272b
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2024-11-27 13:17:46 +0100

    Fixet migration test
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/db/PostgresMigrationTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/db/PostgresMigrationTest.kt
@@ -10,7 +10,7 @@ class PostgresMigrationTest {
     fun `Migration scripts are applied successfully`() {
         withCleanDb {
             val migrations = runMigration()
-            migrations shouldBe 17
+            migrations shouldBe 18
         }
     }
 }
