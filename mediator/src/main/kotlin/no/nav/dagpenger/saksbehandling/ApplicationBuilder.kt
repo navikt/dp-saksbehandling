15afcc861502638ca7d20e633174e89f425fea2b
Geir André Lund <geir.andre.lund@nav.no>
2024-12-05 08:40:42 +0100

    Fjerner utsending av `vedtak_fattet_til_arena` som er erstattet etter omskrivning av dp-arena-sink. dp-arena-sink sender behov om `MeldingOmVedtakProdusent` som blir løst av dp-saksbehandling. (i MeldingOmVedtakProdusentBehovløser)

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/ApplicationBuilder.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/ApplicationBuilder.kt
@@ -123,7 +123,7 @@ internal class ApplicationBuilder(configuration: Map<String, String>) : RapidsCo
         }.also { rapidsConnection ->
             utsendingMediator.setRapidsConnection(rapidsConnection)
             oppgaveMediator.setRapidsConnection(rapidsConnection)
-            VedtakFattetMottak(rapidsConnection, oppgaveMediator, utsendingMediator)
+            VedtakFattetMottak(rapidsConnection, oppgaveMediator)
             BehandlingOpprettetMottak(rapidsConnection, oppgaveMediator, pdlKlient, skjermingKlient)
             BehandlingAvbruttMottak(rapidsConnection, oppgaveMediator)
             ForslagTilVedtakMottak(rapidsConnection, oppgaveMediator)
