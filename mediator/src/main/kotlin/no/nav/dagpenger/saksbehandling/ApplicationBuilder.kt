a2ecf241cef6e01dba9161282eeb60b4c7938fdf
Aurora Christine Hofman <aurora.christine.hofman@nav.no>
2025-02-10 13:49:07 +0100

    Jobb for å slette gamle oppgaver slik at de ikke blir liggende lenge å lage støy i løsningen

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/ApplicationBuilder.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/ApplicationBuilder.kt
@@ -99,6 +99,7 @@ internal class ApplicationBuilder(configuration: Map<String, String>) : RapidsCo
             oppgaveHistorikkDTOMapper = OppgaveHistorikkDTOMapper(oppgaveRepository, saksbehandlerOppslag),
         )
     private val utsendingAlarmJob: Timer
+    private val slettGamleOppgaverJob: Timer
 
     private val rapidsConnection: RapidsConnection =
         RapidApplication.create(
@@ -142,6 +143,7 @@ internal class ApplicationBuilder(configuration: Map<String, String>) : RapidsCo
             )
             MeldingOmVedtakProdusentBehovløser(rapidsConnection, utsendingMediator)
             utsendingAlarmJob = UtsendingAlarmJob(rapidsConnection, UtsendingAlarmRepository(dataSource)).startJob()
+            slettGamleOppgaverJob = SletteGamleOppgaverJob(rapidsConnection, GamleOppgaverRepository(dataSource)).startJob()
         }
 
     init {
---
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/SletteGamleOppgaverJob.kt
@@ -0,0 +1,107 @@
+package no.nav.dagpenger.saksbehandling
+
+import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
+import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
+import kotliquery.queryOf
+import kotliquery.sessionOf
+import mu.KotlinLogging
+import java.time.Instant
+import java.time.ZoneId
+import java.util.Date
+import java.util.Timer
+import java.util.UUID
+import javax.sql.DataSource
+import kotlin.concurrent.fixedRateTimer
+
+private val logger = KotlinLogging.logger {}
+
+internal class SletteGamleOppgaverJob(
+    private val rapidsConnection: RapidsConnection,
+    private val gamleOppgaverRepository: GamleOppgaverRepository,
+) {
+    private val Int.Dag get() = this * 1000L * 60L * 60L * 24L
+
+    fun startJob(): Timer {
+        return fixedRateTimer(
+            name = "SletteGamleOppgaverJob",
+            daemon = true,
+            startAt = Date.from(Instant.now().atZone(ZoneId.of("Europe/Oslo")).toInstant()),
+            period = 1.Dag,
+            action = {
+                try {
+                    logger.info { "Starter SletteGamleOppgaverJob" }
+                    SletteGamleOppgaverJob(
+                        rapidsConnection = rapidsConnection,
+                        gamleOppgaverRepository = gamleOppgaverRepository,
+                    ).avbrytGamleOppgaver()
+                } catch (e: Exception) {
+                    logger.error(e) { "SletteGamleOppgaverJob feilet: ${e.message} " }
+                }
+            },
+        )
+    }
+
+    fun finnGamleOppgaver(): List<GamleOppgaver> {
+        return gamleOppgaverRepository.finnGamleOppgaver(28).also {
+            logger.info { "Fant ${it.size} gamle oppgaver: $it" }
+        }
+    }
+
+    fun avbrytGamleOppgaver() {
+        finnGamleOppgaver().forEach { oppgave ->
+            rapidsConnection.publish(
+                key = oppgave.ident,
+                JsonMessage.newMessage(
+                    eventName = "avbryt_behandling",
+                    map =
+                        mapOf(
+                            "behandlingId" to oppgave.behandlingId,
+                            "søknadId" to oppgave.soknadId,
+                            "ident" to oppgave.ident,
+                        ),
+                ).toJson(),
+            )
+        }
+    }
+}
+
+internal class GamleOppgaverRepository(private val ds: DataSource) {
+    fun finnGamleOppgaver(intervallAntallDager: Int): List<GamleOppgaver> {
+        return sessionOf(ds).use { session ->
+            session.run(
+                queryOf(
+                    //language=PostgreSQL
+                    """
+                    SELECT
+                          person_v1.ident,
+                          oppgave_v1.behandling_id,
+                          hendelse_v1.hendelse_data->>'søknadId' AS søknadId
+                      FROM
+                          oppgave_v1
+                              JOIN
+                          behandling_v1 ON oppgave_v1.behandling_id = behandling_v1.id
+                              JOIN
+                          person_v1 ON behandling_v1.person_id = person_v1.id
+                              JOIN
+                          hendelse_v1 ON hendelse_v1.behandling_id = behandling_v1.id
+                      WHERE
+                          oppgave_v1.endret_tidspunkt < NOW() - INTERVAL '$intervallAntallDager days'
+                        AND oppgave_v1.tilstand NOT IN ('FERDIG_BEHANDLET', 'BEHANDLES_I_ARENA')
+                    """.trimIndent(),
+                ).map { row ->
+                    GamleOppgaver(
+                        ident = row.string("ident"),
+                        soknadId = row.uuid("søknadId"),
+                        behandlingId = row.uuid("behandling_id"),
+                    )
+                }.asList,
+            )
+        }
+    }
+}
+
+data class GamleOppgaver(
+    val ident: String,
+    val soknadId: UUID,
+    val behandlingId: UUID,
+)
