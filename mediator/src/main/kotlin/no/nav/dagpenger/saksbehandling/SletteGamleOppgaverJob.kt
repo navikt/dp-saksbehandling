0d1181c86555ad1a68b62221684a5a41b25073ab
Aurora Christine Hofman <aurora.christine.hofman@nav.no>
2025-02-11 12:51:29 +0100

    Fnuttefeil fix

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/SletteGamleOppgaverJob.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/SletteGamleOppgaverJob.kt
@@ -86,7 +86,7 @@ internal class GamleOppgaverRepository(private val ds: DataSource) {
                           hendelse_v1 ON hendelse_v1.behandling_id = behandling_v1.id
                       WHERE
                           oppgave_v1.opprettet < NOW() - INTERVAL '$intervallAntallDager days'
-                        AND oppgave_v1.tilstand  IN ('OPPRETTET, KLAR_TIL_BEHANDLING', 'PAA_VENT')
+                        AND oppgave_v1.tilstand  IN ('OPPRETTET', 'KLAR_TIL_BEHANDLING', 'PAA_VENT')
                         AND oppgave_v1.saksbehandler_ident IS NULL
                     """.trimIndent(),
                 ).map { row ->
