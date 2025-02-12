41a019acec6def77c21eb7692164f0ebabf8f0b7
Aurora Christine Hofman <aurora.christine.hofman@nav.no>
2025-02-12 10:36:51 +0100

    Feilslettet testfil

---
+++ mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/statistikk/StatistikkTestApiTest.kt
@@ -0,0 +1,34 @@
+package no.nav.dagpenger.saksbehandling.statistikk
+
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.server.testing.testApplication
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.dagpenger.saksbehandling.api.installerApis
+import no.nav.dagpenger.saksbehandling.api.mockAzure
+import org.junit.jupiter.api.Test
+
+class StatistikkTestApiTest {
+    private val mockAzure = mockAzure()
+
+    @Test
+    fun `test public statistikk html response`() {
+        val mockStatistikkTjeneste =
+            mockk<StatistikkTjeneste>().also {
+                every { it.hentAntallBrevSendt() } returns 3
+            }
+        testApplication {
+            application {
+                installerApis(mockk(), mockk(), mockStatistikkTjeneste)
+            }
+
+            client.get("public/statistikk").let { httpResponse ->
+                httpResponse.status.value shouldBe 200
+                httpResponse.bodyAsText() shouldContain """Antall brev sendt: 3"""
+            }
+        }
+    }
+}
