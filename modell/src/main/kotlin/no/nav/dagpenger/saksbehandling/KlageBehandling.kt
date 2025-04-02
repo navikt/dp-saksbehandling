fc72160786ae88e9fb3d643afa9f5f87fac343da
Giao The Cung <giaothe@gmail.com>
2025-04-02 13:52:11 +0200

    WIP: verdens enkleste og styggeste regelmotor
    Co-authored-by: MariusEriksen <Marius.Eriksen@nav.no>
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- modell/src/main/kotlin/no/nav/dagpenger/saksbehandling/KlageBehandling.kt
+++ modell/src/main/kotlin/no/nav/dagpenger/saksbehandling/KlageBehandling.kt
@@ -1,33 +1,133 @@
 package no.nav.dagpenger.saksbehandling
 
 import no.nav.dagpenger.saksbehandling.Utfall.TomtUtfall
+import java.time.LocalDate
 import java.util.UUID
 
 class KlageBehandling(
     val id: UUID,
     val person: Person,
-    val utfall: Utfall = TomtUtfall,
 ) {
+    var utfall: Utfall = TomtUtfall
+
+    fun svar(
+        opplysninngId: UUID,
+        b: Boolean,
+    ) {
+        grupper.flatMap { it.opplysninger }.singleOrNull { it.id == opplysninngId }?.also { opplysning ->
+            opplysning.svar(b)
+            this.revurderUtfall()
+        }
+    }
+
+    private fun revurderUtfall() {
+        formkravGruppe.opplysninger.filter { it.verdi is Verdi.Boolse }.any {
+            !(it.verdi as Verdi.Boolse).value
+        }.let {
+            when (it) {
+                true -> this.utfall = Utfall.Avvist
+                false -> {}
+            }
+        }
+    }
+
+    val klagesakGruppe =
+        Gruppe(
+            GrupperNavn.KLAGESAK,
+            setOf(
+                Opplysning(
+                    navn = "Hva klagen gjelder",
+                    type = Opplysning.OpplysningType.TEKST,
+                    verdi = Verdi.TomVerdi,
+                ),
+                Opplysning(
+                    navn = "Vedtak klagen gjelder",
+                    type = Opplysning.OpplysningType.TEKST,
+                    verdi = Verdi.TomVerdi,
+                ),
+            ),
+        )
+    val fristGruppe =
+        Gruppe(
+            GrupperNavn.FRIST,
+            setOf(
+                Opplysning(
+                    navn = "Frist for å klage",
+                    type = Opplysning.OpplysningType.DATO,
+                    verdi = Verdi.TomVerdi,
+                ),
+                Opplysning(
+                    navn = "Frist mottatt",
+                    type = Opplysning.OpplysningType.DATO,
+                    verdi = Verdi.TomVerdi,
+                ),
+                Opplysning(
+                    navn = "Har klager klaget innen fristen",
+                    type = Opplysning.OpplysningType.BOOLSK,
+                    verdi = Verdi.TomVerdi,
+                ),
+            ),
+        )
+
+    val formkravGruppe =
+        Gruppe(
+            GrupperNavn.FORMKRAV,
+            setOf(
+                Opplysning(
+                    navn = "Er klagen skriftlig",
+                    type = Opplysning.OpplysningType.BOOLSK,
+                    verdi = Verdi.TomVerdi,
+                ),
+                Opplysning(
+                    navn = "Er klagen underskrevet",
+                    type = Opplysning.OpplysningType.BOOLSK,
+                    verdi = Verdi.TomVerdi,
+                ),
+                Opplysning(
+                    navn = "Nevner klagen den endring som krevest",
+                    type = Opplysning.OpplysningType.BOOLSK,
+                    verdi = Verdi.TomVerdi,
+                ),
+                Opplysning(
+                    navn = "Har klager rettslig klageinteresse",
+                    type = Opplysning.OpplysningType.BOOLSK,
+                    verdi = Verdi.TomVerdi,
+                ),
+            ),
+        )
+
     val grupper: Set<Gruppe> =
         setOf(
-            Gruppe(GrupperNavn.FORMKRAV, emptySet()),
-            Gruppe(GrupperNavn.KLAGESAK, emptySet()),
-            Gruppe(GrupperNavn.FRIST, emptySet()),
+            klagesakGruppe,
+            formkravGruppe,
+            fristGruppe,
         )
 }
 
 class Opplysning(
-    val id: UUID,
+    val id: UUID = UUIDv7.ny(),
     val navn: String,
-    val verdi: Verdi = Verdi.TomVerdi,
-    val gruppe: Gruppe,
-)
+    val type: OpplysningType,
+    var verdi: Verdi,
+) {
+    fun svar(b: Boolean) {
+        when (type) {
+            OpplysningType.BOOLSK -> verdi = Verdi.Boolse(b)
+            else -> throw IllegalArgumentException("Kan ikke svare på en opplysning av type $type")
+        }
+    }
+
+    enum class OpplysningType {
+        TEKST,
+        DATO,
+        BOOLSK,
+    }
+}
 
 enum class GrupperNavn {
     FORMKRAV,
     KLAGESAK,
     FRIST,
-    KLAGE_ANKE,
 }
 
 class Gruppe(
@@ -35,10 +135,18 @@ class Gruppe(
     val opplysninger: Set<Opplysning>,
 )
 
-sealed class Verdi {
+sealed class Verdi() {
     object TomVerdi : Verdi()
+
+    data class TekstVerdi(val value: String) : Verdi()
+
+    data class Dato(val value: LocalDate) : Verdi()
+
+    data class Boolse(val value: Boolean) : Verdi()
 }
 
 sealed class Utfall {
     object TomtUtfall : Utfall()
+
+    object Avvist : Utfall()
 }
--- modell/src/test/kotlin/no/nav/dagpenger/saksbehandling/KlageBehandlingTest.kt
+++ modell/src/test/kotlin/no/nav/dagpenger/saksbehandling/KlageBehandlingTest.kt
@@ -1,5 +1,6 @@
 package no.nav.dagpenger.saksbehandling
 
+import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.shouldBe
 import no.nav.dagpenger.saksbehandling.GrupperNavn.FORMKRAV
 import no.nav.dagpenger.saksbehandling.GrupperNavn.FRIST
@@ -19,6 +20,12 @@ class KlageBehandlingTest {
                         adressebeskyttelseGradering = AdressebeskyttelseGradering.UGRADERT,
                     ),
             )
-        klageBehandling.grupper.map { it.navn } shouldBe setOf(FORMKRAV, KLAGESAK, FRIST)
+
+        klageBehandling.grupper.map { it.navn } shouldContainAll listOf(FORMKRAV, KLAGESAK, FRIST)
+        val opplysninngId = klageBehandling.grupper.single { it.navn == FORMKRAV }.opplysninger.first().id
+
+        // do someting
+        klageBehandling.svar(opplysninngId, false)
+        klageBehandling.utfall shouldBe Utfall.Avvist
     }
 }
