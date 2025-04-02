082cd754d1a93cc746ad1077068383b4ac8a47bf
Giao The Cung <giaothe@gmail.com>
2025-04-02 10:47:01 +0200

    WIP:Ferdigstill enkle tilstands endring for klage oppgave
    Co-authored-by: MariusEriksen <Marius.Eriksen@nav.no>
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

---
+++ modell/src/main/kotlin/no/nav/dagpenger/saksbehandling/KlageOppgave.kt
@@ -0,0 +1,77 @@
+package no.nav.dagpenger.saksbehandling
+
+import no.nav.dagpenger.saksbehandling.hendelser.FerdigstillKlageOppgave
+import no.nav.dagpenger.saksbehandling.hendelser.SettOppgaveAnsvarHendelse
+import java.time.LocalDateTime
+import java.util.UUID
+
+class KlageOppgave(
+    oppgaveId: UUID,
+    opprettet: LocalDateTime,
+) : GeneriskOppgave(
+        oppgaveId = oppgaveId,
+        opprettet = opprettet,
+        _emneknagger = mutableSetOf(),
+    ) {
+    private var tilstand: Tilstand = KlarTilBehandling
+
+    fun tilstand() = this.tilstand
+
+    fun tildel(settOppgaveAnsvarHendelse: SettOppgaveAnsvarHendelse) {
+        tilstand.tildel(this, settOppgaveAnsvarHendelse)
+    }
+
+    fun ferdigstill(ferdigstillKlageOppgave: FerdigstillKlageOppgave) {
+        tilstand.ferdigstill(this, ferdigstillKlageOppgave)
+    }
+
+    sealed interface Tilstand {
+        fun tildel(
+            klageOppgave: KlageOppgave,
+            settOppgaveAnsvarHendelse: SettOppgaveAnsvarHendelse,
+        ) {
+            throw IllegalStateException("Kan ikke tildele oppgave i denne tilstanden")
+        }
+
+        fun ferdigstill(
+            klageOppgave: KlageOppgave,
+            settOppgaveAnsvarHendelse: FerdigstillKlageOppgave,
+        ) {
+            throw IllegalStateException("Kan ikke tildele oppgave i denne tilstanden")
+        }
+
+        val type: Type
+
+        enum class Type {
+            KLAR_TIL_BEHANDLING,
+            UNDER_BEHANDLING,
+            FERDIG_BEHANDLET,
+        }
+    }
+
+    data object KlarTilBehandling : Tilstand {
+        override val type: Tilstand.Type = Tilstand.Type.KLAR_TIL_BEHANDLING
+
+        override fun tildel(
+            klageOppgave: KlageOppgave,
+            settOppgaveAnsvarHendelse: SettOppgaveAnsvarHendelse,
+        ) {
+            klageOppgave.tilstand = UnderBehandling
+        }
+    }
+
+    data object UnderBehandling : Tilstand {
+        override val type: Tilstand.Type = Tilstand.Type.UNDER_BEHANDLING
+
+        override fun ferdigstill(
+            klageOppgave: KlageOppgave,
+            settOppgaveAnsvarHendelse: FerdigstillKlageOppgave,
+        ) {
+            klageOppgave.tilstand = FerdigBehandlet
+        }
+    }
+
+    data object FerdigBehandlet : Tilstand {
+        override val type: Tilstand.Type = Tilstand.Type.FERDIG_BEHANDLET
+    }
+}
--- modell/src/test/kotlin/no/nav/dagpenger/saksbehandling/KlageOppgaveTest.kt
+++ modell/src/test/kotlin/no/nav/dagpenger/saksbehandling/KlageOppgaveTest.kt
@@ -1,16 +1,42 @@
 package no.nav.dagpenger.saksbehandling
 
 import io.kotest.matchers.shouldBe
-import no.nav.dagpenger.saksbehandling.KlageOppgave.Opprettet
+import no.nav.dagpenger.saksbehandling.hendelser.FerdigstillKlageOppgave
+import no.nav.dagpenger.saksbehandling.hendelser.SettOppgaveAnsvarHendelse
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
+import java.util.UUID
 
 class KlageOppgaveTest {
+    private val saksbehandler =
+        Saksbehandler(
+            navIdent = "Z123",
+            grupper = emptySet(),
+            tilganger = emptySet(),
+        )
+
+    private val oppgaveId: UUID = UUIDv7.ny()
+
     @Test
-    fun `Opprettelse av KlageOppgave har tilstand Opprettet`() {
-        KlageOppgave(
-            oppgaveId = UUIDv7.ny(),
-            opprettet = LocalDateTime.now(),
-        ).tilstand() shouldBe Opprettet
+    fun `Livsyklus på klage oppgavens tilstander`() {
+        val klageOppgave =
+            KlageOppgave(
+                oppgaveId = UUIDv7.ny(),
+                opprettet = LocalDateTime.now(),
+            ).also {
+                it.tilstand() shouldBe KlageOppgave.KlarTilBehandling
+            }
+
+        klageOppgave.tildel(
+            SettOppgaveAnsvarHendelse(
+                oppgaveId = oppgaveId,
+                ansvarligIdent = saksbehandler.navIdent,
+                utførtAv = saksbehandler,
+            ),
+        )
+        klageOppgave.tilstand() shouldBe KlageOppgave.UnderBehandling
+
+        klageOppgave.ferdigstill(FerdigstillKlageOppgave(utførtAv = saksbehandler))
+        klageOppgave.tilstand() shouldBe KlageOppgave.FerdigBehandlet
     }
 }
