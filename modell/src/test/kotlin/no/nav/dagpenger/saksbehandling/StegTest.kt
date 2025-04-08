3b7ad19ba94cb17d4df9a8c6017c75eb0953cda8
Marius Eriksen <marius.eriksen@nav.no>
2025-04-08 14:04:16 +0200

    wip: Mer steg moro. Stegosaurus
    Co-authored-by: Giao The Cung <giaothe@gmail.com>
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- modell/src/test/kotlin/no/nav/dagpenger/saksbehandling/StegTest.kt
+++ modell/src/test/kotlin/no/nav/dagpenger/saksbehandling/StegTest.kt
@@ -10,11 +10,19 @@ class StegTest {
         val steg = FristvurderingSteg()
 
         val klagefristOppfylt = steg.opplysninger().single { it.type == KLAGEFRIST_OPPFYLT }
+
         klagefristOppfylt.svar(verdi = true)
         steg.opplysninger().size shouldBe 3
         (klagefristOppfylt.verdi as Verdi.Boolsk).value shouldBe true
+
         klagefristOppfylt.svar(verdi = false)
         steg.opplysninger().size shouldBe 5
         (klagefristOppfylt.verdi as Verdi.Boolsk).value shouldBe false
+        val opplysning = steg.opplysninger().single { it.type == OpplysningType.OPPREISNING_OVERSITTET_FRIST }
+        opplysning.svar(verdi = false)
+
+        klagefristOppfylt.svar(verdi = true)
+        steg.opplysninger().size shouldBe 3
+        //TODO: Når klagefrist er oppfylt skal ikke oversittet frist opplysninger ha verdi
     }
 }
