6f25af5fa67ad1aa00c37053b3a4610bac38af66
Giao The Cung <giaothe@gmail.com>
2025-04-15 10:04:18 +0200

    Begynne på mediator test
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/KlageMediatorTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/KlageMediatorTest.kt
@@ -1,21 +1,55 @@
 package no.nav.dagpenger.saksbehandling
 
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.mockk.coEvery
 import io.mockk.mockk
+import no.nav.dagpenger.saksbehandling.AdressebeskyttelseGradering.UGRADERT
+import no.nav.dagpenger.saksbehandling.api.Oppslag
+import no.nav.dagpenger.saksbehandling.db.Postgres.withMigratedDb
 import no.nav.dagpenger.saksbehandling.db.klage.InmemoryKlageRepository
-import no.nav.dagpenger.saksbehandling.db.klage.InmemoryKlageRepository.testKlageId1
+import no.nav.dagpenger.saksbehandling.db.person.PostgresPersonRepository
+import no.nav.dagpenger.saksbehandling.hendelser.KlageMottattHendelse
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 
 class KlageMediatorTest {
+    private val testPersonIdent = "12345678901"
+
+    private val oppslagMock =
+        mockk<Oppslag>().also {
+            coEvery { it.hentPersonMedSkjermingOgGradering(testPersonIdent) } returns
+                Person(
+                    id = UUIDv7.ny(),
+                    ident = testPersonIdent,
+                    skjermesSomEgneAnsatte = false,
+                    adressebeskyttelseGradering = UGRADERT,
+                )
+        }
+
     @Test
-    fun `hent klage returnerer en dummyklage`() {
-        val klageMediator =
-            KlageMediator(
-                klageRepository = InmemoryKlageRepository,
-                personRepository = mockk(),
-                oppslag = mockk(),
+    fun `livsyklus til en klage`() {
+        withMigratedDb { ds ->
+            val klageMediator =
+                KlageMediator(
+                    klageRepository = InmemoryKlageRepository,
+                    personRepository = PostgresPersonRepository(ds),
+                    oppslag = oppslagMock,
+                )
+
+            klageMediator.opprettKlage(
+                KlageMottattHendelse(
+                    ident = testPersonIdent,
+                    opprettet = LocalDateTime.now(),
+                    journalpostId = "journalpostId",
+                ),
             )
-        val klageOppgave = klageMediator.hentKlageOppgave(testKlageId1)
-        klageOppgave.oppgaveId shouldBe testKlageId1
+
+            val klageOppgave =
+                InmemoryKlageRepository.hentKlager().single {
+                    it.klageBehandling.person.ident == testPersonIdent
+                }
+
+            klageOppgave shouldNotBe null
+        }
     }
 }
--- modell/src/main/kotlin/no/nav/dagpenger/saksbehandling/hendelser/KlageMottattHendelse.kt
+++ modell/src/main/kotlin/no/nav/dagpenger/saksbehandling/hendelser/KlageMottattHendelse.kt
@@ -2,12 +2,9 @@ package no.nav.dagpenger.saksbehandling.hendelser
 
 import no.nav.dagpenger.saksbehandling.Applikasjon
 import no.nav.dagpenger.saksbehandling.Behandler
-import no.nav.dagpenger.saksbehandling.UUIDv7
 import java.time.LocalDateTime
-import java.util.UUID
 
 data class KlageMottattHendelse(
-    val klageId: UUID = UUIDv7.ny(),
     val ident: String,
     val opprettet: LocalDateTime,
     val journalpostId: String,
