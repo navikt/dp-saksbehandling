23f96ac53136ae3f00772316e96e3e37939f0fb7
Giao The Cung <giaothe@gmail.com>
2025-05-14 10:39:01 +0200

    Endre filter

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/db/oppgave/PostgresOppgaveRepository.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/db/oppgave/PostgresOppgaveRepository.kt
@@ -515,6 +515,10 @@ class PostgresOppgaveRepository(private val datasource: DataSource) :
                     tilstander = Type.søkbareTilstander,
                     saksbehandlerIdent = null,
                     personIdent = ident,
+                    paginering = Søkefilter.Paginering(
+                        antallOppgaver = 50,
+                        side = 0,
+                    )
                 ),
         ).oppgaver
 
