9bab3149943eff1c9a23f87cbcfb59aa33283bc0
Giao The Cung <giaothe@gmail.com>
2025-06-13 10:08:55 +0200

    Skal ikke opprette personer som er adressebeskyttet eller skjermet
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/db/person/PersonMediatorTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/db/person/PersonMediatorTest.kt
@@ -1,19 +1,19 @@
 package no.nav.dagpenger.saksbehandling.db.person
 
+import AdresseBeeskyttetPersonException
 import PersonMediator
 import SkjermetPersonException
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.matchers.ints.exactly
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import no.nav.dagpenger.saksbehandling.AdressebeskyttelseGradering.FORTROLIG
+import no.nav.dagpenger.saksbehandling.AdressebeskyttelseGradering.STRENGT_FORTROLIG
 import no.nav.dagpenger.saksbehandling.AdressebeskyttelseGradering.UGRADERT
-import no.nav.dagpenger.saksbehandling.Person
 import no.nav.dagpenger.saksbehandling.api.Oppslag
 import org.junit.jupiter.api.Test
-import java.util.UUID
 
 class PersonMediatorTest {
     private val testIdent = "12345678901"
@@ -21,7 +21,7 @@ class PersonMediatorTest {
     private val personRepositoryMock =
         mockk<PersonRepository>(relaxed = true).also {
             every {
-                it.finnPerson(any<UUID>())
+                it.finnPerson(any<String>())
             } returns null
         }
 
@@ -29,28 +29,47 @@ class PersonMediatorTest {
 
     @Test
     fun `Skal ikke opprette person hvis personen er skjermet`() {
-        val erSkjermetIdent = "12345678901"
-        val ikkeSkjermet = "12345678902"
-        coEvery { oppslagMock.erAdressebeskyttetPerson(testIdent) } returns UGRADERT
-        coEvery { oppslagMock.erSkjermetPerson(erSkjermetIdent) } returns true
-        coEvery { oppslagMock.erSkjermetPerson(ikkeSkjermet) } returns false
+        val skjermetIdent = "12345678901"
+        val ikkeSkjermetIdent = "12345678902"
+        coEvery { oppslagMock.erAdressebeskyttetPerson(any()) } returns UGRADERT
+        coEvery { oppslagMock.erSkjermetPerson(skjermetIdent) } returns true
+        coEvery { oppslagMock.erSkjermetPerson(ikkeSkjermetIdent) } returns false
 
         val personMediator = PersonMediator(personRepositoryMock, oppslagMock)
         shouldThrow<SkjermetPersonException> {
-            personMediator.finnEllerOpprettPerson(erSkjermetIdent)
+            personMediator.finnEllerOpprettPerson(skjermetIdent)
         }
 
-        shouldNotThrowAny { personMediator.finnEllerOpprettPerson(ikkeSkjermet) }
+        shouldNotThrowAny { personMediator.finnEllerOpprettPerson(ikkeSkjermetIdent) }
 
         verify(exactly = 1) {
-            personRepositoryMock.lagre(
-                Person(
-                    id = any(),
-                    ident = ikkeSkjermet,
-                    skjermesSomEgneAnsatte = false,
-                    adressebeskyttelseGradering = UGRADERT,
-                ),
-            )
+            personRepositoryMock.lagre(match { person -> person.ident == ikkeSkjermetIdent })
+        }
+    }
+
+    @Test
+    fun `Skal ikke opprette person hvis personen er addressebeskyttet`() {
+        val fortroligBeskyttetIdent = "12345678901"
+        val strengtFortroligBeskyttetIdent = "12345678903"
+        val ikkeAddressebeskyttetIdent = "12345678902"
+        coEvery { oppslagMock.erAdressebeskyttetPerson(strengtFortroligBeskyttetIdent) } returns STRENGT_FORTROLIG
+        coEvery { oppslagMock.erAdressebeskyttetPerson(fortroligBeskyttetIdent) } returns FORTROLIG
+        coEvery { oppslagMock.erAdressebeskyttetPerson(ikkeAddressebeskyttetIdent) } returns UGRADERT
+        coEvery { oppslagMock.erSkjermetPerson(any()) } returns false
+
+        val personMediator = PersonMediator(personRepositoryMock, oppslagMock)
+        shouldThrow<AdresseBeeskyttetPersonException> {
+            personMediator.finnEllerOpprettPerson(fortroligBeskyttetIdent)
+        }
+
+        shouldThrow<AdresseBeeskyttetPersonException> {
+            personMediator.finnEllerOpprettPerson(strengtFortroligBeskyttetIdent)
+        }
+
+        shouldNotThrowAny { personMediator.finnEllerOpprettPerson(ikkeAddressebeskyttetIdent) }
+
+        verify(exactly = 1) {
+            personRepositoryMock.lagre(match { person -> person.ident == ikkeAddressebeskyttetIdent })
         }
     }
 }
