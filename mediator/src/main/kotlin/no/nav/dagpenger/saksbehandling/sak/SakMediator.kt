b7ba397fe59ac87db442ef1fad9e69d38f6842ce
Giao The Cung <giaothe@gmail.com>
2025-06-13 10:24:44 +0200

    Publiser avbryt hendelse dersom vi ikke får opprettet sak for en søknad der personene er adressebeskyttet/skjermet
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/sak/SakMediator.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/sak/SakMediator.kt
@@ -1,6 +1,10 @@
 package no.nav.dagpenger.saksbehandling.sak
 
+import AdresseBeeskyttetPersonException
 import PersonMediator
+import SkjermetPersonException
+import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
+import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
 import no.nav.dagpenger.saksbehandling.BehandlingType
 import no.nav.dagpenger.saksbehandling.NyBehandling
 import no.nav.dagpenger.saksbehandling.NySak
@@ -9,9 +13,11 @@ import no.nav.dagpenger.saksbehandling.db.sak.SakRepository
 import no.nav.dagpenger.saksbehandling.hendelser.MeldekortbehandlingOpprettetHendelse
 import no.nav.dagpenger.saksbehandling.hendelser.SøknadsbehandlingOpprettetHendelse
 
+private val logger = mu.KotlinLogging.logger {}
 class SakMediator(
     private val personMediator: PersonMediator,
     private val sakRepository: SakRepository,
+    private val rapidsConnection: RapidsConnection,
 ) {
     fun hentSakHistorikk(ident: String): SakHistorikk {
         return sakRepository.hentSakHistorikk(ident)
@@ -36,13 +42,23 @@ class SakMediator(
                 )
             }
 
-        val sakHistorikk =
-            sakRepository.finn(søknadsbehandlingOpprettetHendelse.ident) ?: SakHistorikk(
-                person = personMediator.finnEllerOpprettPerson(søknadsbehandlingOpprettetHendelse.ident),
-            )
-
-        sakHistorikk.leggTilSak(sak)
-        sakRepository.lagre(sakHistorikk)
+        kotlin.runCatching {
+            personMediator.finnEllerOpprettPerson(søknadsbehandlingOpprettetHendelse.ident)
+        }.onFailure { e ->
+            when (e is AdresseBeeskyttetPersonException || e is SkjermetPersonException) {
+                true -> {
+                    sendAvbrytBehandling( søknadsbehandlingOpprettetHendelse = søknadsbehandlingOpprettetHendelse)
+                }
+                else -> { throw e }
+            }
+        }.onSuccess { person ->
+            val sakHistorikk =
+                sakRepository.finn(søknadsbehandlingOpprettetHendelse.ident) ?: SakHistorikk(
+                    person = person,
+                )
+            sakHistorikk.leggTilSak(sak)
+            sakRepository.lagre(sakHistorikk)
+        }
     }
 
     fun knyttTilSak(meldekortbehandlingOpprettetHendelse: MeldekortbehandlingOpprettetHendelse) {
@@ -50,5 +66,22 @@ class SakMediator(
             it.knyttTilSak(meldekortbehandlingOpprettetHendelse)
             sakRepository.lagre(it)
         }
+    }
+
+    private fun  sendAvbrytBehandling(
+        søknadsbehandlingOpprettetHendelse: SøknadsbehandlingOpprettetHendelse
+    ) {
+        rapidsConnection.publish(
+            key = søknadsbehandlingOpprettetHendelse.ident,
+            message =  JsonMessage.newMessage(
+                eventName = "avbryt_behandling",
+                map = mapOf(
+                    "behandlingId" to søknadsbehandlingOpprettetHendelse.behandlingId,
+                    "søknadId" to søknadsbehandlingOpprettetHendelse.søknadId,
+                    "ident" to søknadsbehandlingOpprettetHendelse.ident,
+                )
+            ).toJson()
+        )
+        logger.info { "Publiserte avbryt_behandling hendelse" }
     }
 }
