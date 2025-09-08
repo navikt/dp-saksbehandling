e44fde1739b80f90927fecf4905db02d3eaf7121
Giao The Cung <giaothe@gmail.com>
2025-09-08 21:00:52 +0200

    Parsen emneknagger fra behandling resultat

---
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/mottak/EmneknaggBuilder.kt
@@ -0,0 +1,154 @@
+package no.nav.dagpenger.saksbehandling.mottak
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import io.github.oshai.kotlinlogging.KotlinLogging
+import no.nav.dagpenger.saksbehandling.Emneknagg
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_ALDER
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_ANDRE_YTELSER
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_ARBEIDSINNTEKT
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_ARBEIDSTID
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_IKKE_REGISTRERT
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_MEDLEMSKAP
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_MINSTEINNTEKT
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_OPPHOLD_UTLAND
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_REELL_ARBEIDSSØKER
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_STREIK
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_UTDANNING
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_UTESTENGT
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.INNVILGELSE
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.RETTIGHET_KONKURS
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.RETTIGHET_ORDINÆR
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.RETTIGHET_PERMITTERT
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.RETTIGHET_PERMITTERT_FISK
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.RETTIGHET_VERNEPLIKT
+import no.nav.dagpenger.saksbehandling.mottak.OpplysningTyper.RETTIGHET_DAGPEGNER_UNDER_PERMITTERING
+import no.nav.dagpenger.saksbehandling.mottak.OpplysningTyper.RETTIGHET_DAGPENGER_ETTER_KONKURS
+import no.nav.dagpenger.saksbehandling.mottak.OpplysningTyper.RETTIGHET_DAGPENGER_ETTER_VERNEPLIKT
+import no.nav.dagpenger.saksbehandling.mottak.OpplysningTyper.RETTIGHET_DAGPENGER_UNDER_PERMITTERING_I_FISKEFOREDLINGSINDUSTRI
+import no.nav.dagpenger.saksbehandling.mottak.OpplysningTyper.RETTIGHET_ORDINÆRE_DAGPENGER
+
+private val logger = KotlinLogging.logger { }
+private val sikkerLogg = KotlinLogging.logger("tjenestekall")
+
+class EmneknaggBuilder(
+    json: String,
+) {
+    private val objectMapper =
+        ObjectMapper().also {
+            it.registerModule(JavaTimeModule())
+        }
+
+    private val jsonNode =
+        try {
+            objectMapper.readTree(json)
+        } catch (e: Exception) {
+            logger.error(e) { "Kunne ikke parse json: ${e.message}" }
+            sikkerLogg.error { "Kunne ikke parse json: $json" }
+            throw IllegalArgumentException("Kunne ikke parse json: $e")
+        }.also {
+            require(it.isObject) {
+                sikkerLogg.error { "Forventet at json er et objekt: $it" }
+            }
+        }
+
+    private val opplysningNode =
+        jsonNode["opplysninger"].also {
+            require(it.isPresent() && it.isArray) {
+                sikkerLogg.error { "Kunne ikke parse opplysninger: $it" }
+                "Forventet at opplysninger er en liste"
+            }
+        }
+
+    private val rettighetsPerioder =
+        jsonNode["rettighetsperioder"].also {
+            require(it.isPresent() && it.isArray) {
+                sikkerLogg.error { "Kunne ikke parse rettighetsperioder: $it" }
+                "Forventet at rettighetsperioder er en liste"
+            }
+        }
+
+    private val behandletHendelse =
+        jsonNode["behandletHendelse"].also {
+            require(it.isPresent() && it.isObject) {
+                sikkerLogg.error { "Kunne ikke parse behandletHendelse: $it" }
+                "Forventet at behandletHendelse er et objekt"
+            }
+        }
+
+    private val rettighetTilEmneknagg =
+        mapOf(
+            RETTIGHET_ORDINÆRE_DAGPENGER.opplysningTypeId to RETTIGHET_ORDINÆR.visningsnavn,
+            RETTIGHET_DAGPENGER_ETTER_VERNEPLIKT.opplysningTypeId to RETTIGHET_VERNEPLIKT.visningsnavn,
+            RETTIGHET_DAGPEGNER_UNDER_PERMITTERING.opplysningTypeId to RETTIGHET_PERMITTERT.visningsnavn,
+            RETTIGHET_DAGPENGER_UNDER_PERMITTERING_I_FISKEFOREDLINGSINDUSTRI.opplysningTypeId to
+                RETTIGHET_PERMITTERT_FISK.visningsnavn,
+            RETTIGHET_DAGPENGER_ETTER_KONKURS.opplysningTypeId to RETTIGHET_KONKURS.visningsnavn,
+        )
+
+    private val rettighetOpplysningId = rettighetTilEmneknagg.keys
+
+    private val avslagTilEmneknagg =
+        mapOf(
+            OpplysningTyper.OPPFYLLER_KRAV_TIL_MINSTEINNTEKT.opplysningTypeId to AVSLAG_MINSTEINNTEKT.visningsnavn,
+            OpplysningTyper.KRAV_TIL_TAP_AV_ARBEIDSINNTEKT.opplysningTypeId to AVSLAG_ARBEIDSINNTEKT.visningsnavn,
+            OpplysningTyper.TAP_AV_ARBEIDSTID_ER_MINST_TERSKEL.opplysningTypeId to AVSLAG_ARBEIDSTID.visningsnavn,
+            OpplysningTyper.KRAV_TIL_ALDER.opplysningTypeId to AVSLAG_ALDER.visningsnavn,
+            OpplysningTyper.IKKE_FULLE_YTELSER.opplysningTypeId to AVSLAG_ANDRE_YTELSER.visningsnavn,
+            OpplysningTyper.OPPFYLLER_MEDLEMSKAP.opplysningTypeId to AVSLAG_MEDLEMSKAP.visningsnavn,
+            OpplysningTyper.IKKE_PÅVIRKET_AV_STREIK_ELLER_LOCKOUT.opplysningTypeId to AVSLAG_STREIK.visningsnavn,
+            OpplysningTyper.OPPFYLLER_KRAVET_OPPHOLD.opplysningTypeId to AVSLAG_OPPHOLD_UTLAND.visningsnavn,
+            OpplysningTyper.KRAV_TIL_ARBEIDSSØKER.opplysningTypeId to AVSLAG_REELL_ARBEIDSSØKER.visningsnavn,
+            OpplysningTyper.OPPYLLER_KRAV_TIL_REGISTRERT_ARBEIDSSØKER.opplysningTypeId to AVSLAG_IKKE_REGISTRERT.visningsnavn,
+            OpplysningTyper.OPPFYLLER_KRAV_TIL_IKKE_UTESTENGT.opplysningTypeId to AVSLAG_UTESTENGT.visningsnavn,
+            OpplysningTyper.KRAV_TIL_UTDANNING_ELLER_OPPLÆRING.opplysningTypeId to AVSLAG_UTDANNING.visningsnavn,
+        )
+
+    private val avslagOpplysningId = avslagTilEmneknagg.keys
+
+    fun bygg(): Set<String> {
+        return buildSet {
+            addAll(rettighetEmneknagger())
+            when (utfall()) {
+                true -> add(INNVILGELSE.visningsnavn)
+                false -> {
+                    add(Emneknagg.Regelknagg.AVSLAG.visningsnavn)
+                    addAll(avslagEmneknagger())
+                }
+            }
+            val hendelseType = behandletHendelse["type"].asText()
+            when (hendelseType) {
+                "Meldekort" -> add(Emneknagg.BehandletHendelseType.MELDEKORT.visningsnavn)
+                "Manuell" -> add(Emneknagg.BehandletHendelseType.MANUELL.visningsnavn)
+            }
+        }
+    }
+
+    private fun avslagEmneknagger(): Set<String> {
+        return opplysningNode.filter { it["opplysningTypeId"].asUUID() in avslagOpplysningId }
+            .filter { it["perioder"].any { periode -> !periode["verdi"].boolskVerdi() } }
+            .mapNotNull { avslagTilEmneknagg[it["opplysningTypeId"].asUUID()] }
+            .toSet()
+    }
+
+    private fun rettighetEmneknagger(): Set<String> {
+        return opplysningNode.filter { it["opplysningTypeId"].asUUID() in rettighetOpplysningId }
+            .filter { it["perioder"].any { periode -> periode["verdi"].boolskVerdi() } }
+            .mapNotNull { rettighetTilEmneknagg[it["opplysningTypeId"].asUUID()] }
+            .toSet()
+    }
+
+    private fun utfall(): Boolean {
+        return rettighetsPerioder.any { it["harRett"].asBoolean() }
+    }
+
+    private fun JsonNode.boolskVerdi(): Boolean {
+        return this["datatype"].asText() == "boolsk" && this["verdi"].asBoolean()
+    }
+
+    private fun JsonNode?.isPresent(): Boolean {
+        if (this == null) return false
+        return !(this.isMissingNode || this.isNull)
+    }
+}
---
+++ mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/mottak/EmneknaggBuilderTest.kt
@@ -0,0 +1,459 @@
+package no.nav.dagpenger.saksbehandling.mottak
+
+import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import no.nav.dagpenger.saksbehandling.Emneknagg.BehandletHendelseType
+import no.nav.dagpenger.saksbehandling.Emneknagg.BehandletHendelseType.MELDEKORT
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_ALDER
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_ANDRE_YTELSER
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_ARBEIDSINNTEKT
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_ARBEIDSTID
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_IKKE_REGISTRERT
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_MEDLEMSKAP
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_MINSTEINNTEKT
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_OPPHOLD_UTLAND
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_REELL_ARBEIDSSØKER
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_STREIK
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_UTDANNING
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG_UTESTENGT
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.INNVILGELSE
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.RETTIGHET_ORDINÆR
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.RETTIGHET_PERMITTERT
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.RETTIGHET_VERNEPLIKT
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+class EmneknaggBuilderTest {
+    @Test
+    fun `Feil eller mangler ved json`() {
+        shouldThrow<IllegalArgumentException> {
+            EmneknaggBuilder(json = "")
+        }
+
+        shouldThrow<IllegalArgumentException> {
+            EmneknaggBuilder(json = "{}")
+        }
+        shouldThrow<IllegalArgumentException> {
+            //language=JSON
+            EmneknaggBuilder("""{ "opplysninger": [] }""")
+        }
+
+        shouldThrow<IllegalArgumentException> {
+            //language=JSON
+            EmneknaggBuilder("""{ "rettighetsperioder": [] }""")
+        }
+
+        shouldThrow<IllegalArgumentException> {
+            //language=JSON
+            EmneknaggBuilder("""{ "behandletHendelse":{} }""")
+        }
+
+        shouldNotThrowAny {
+            //language=JSON
+            EmneknaggBuilder(
+                """
+                {
+                  "behandletHendelse": { "type": "Søknad" },
+                  "rettighetsperioder": [],
+                  "opplysninger": []
+                }
+                """.trimIndent(),
+            )
+        }
+    }
+
+    @Test
+    fun `Riktig håndtering av emneknagger der det finnes flere opplysninger av samme type`() {
+        lagBehandlingResultat(
+            behandletHendelseType = "Søknad",
+            harRettighet = true,
+            ForenkletOpplysning(
+                id = OpplysningTyper.RETTIGHET_ORDINÆRE_DAGPENGER.opplysningTypeId,
+                verdi = true,
+            ),
+            ForenkletOpplysning(
+                id = OpplysningTyper.RETTIGHET_ORDINÆRE_DAGPENGER.opplysningTypeId,
+                verdi = false,
+            ),
+        ).also {
+            EmneknaggBuilder(it).bygg() shouldBe setOf(INNVILGELSE.visningsnavn, RETTIGHET_ORDINÆR.visningsnavn)
+        }
+
+        lagBehandlingResultat(
+            behandletHendelseType = "Søknad",
+            harRettighet = true,
+            ForenkletOpplysning(
+                id = OpplysningTyper.OPPFYLLER_KRAV_TIL_IKKE_UTESTENGT.opplysningTypeId,
+                verdi = false,
+            ),
+            ForenkletOpplysning(
+                id = OpplysningTyper.OPPFYLLER_KRAV_TIL_IKKE_UTESTENGT.opplysningTypeId,
+                verdi = true,
+            ),
+        ).also {
+            EmneknaggBuilder(it).bygg() shouldBe setOf(INNVILGELSE.visningsnavn)
+        }
+    }
+
+    @Test
+    fun `Skal sette riktig innvigelse eller avslag emneknagg  behandlinger med flere rettighetsperioder`() {
+        //language=JSON
+        EmneknaggBuilder(
+            """
+                {
+                  "behandletHendelse": { "type": "Søknad" },
+                  "rettighetsperioder": [{"harRett": false}, {"harRett": true}, {"harRett": false}],
+                  "opplysninger": []
+                }
+            """,
+        ).bygg() shouldBe setOf(INNVILGELSE.visningsnavn)
+
+        //language=JSON
+        EmneknaggBuilder(
+            """
+                {
+                  "behandletHendelse": { "type": "Søknad" },
+                  "rettighetsperioder": [{"harRett": false}, {"harRett": false}],
+                  "opplysninger": []
+                }
+            """,
+        ).bygg() shouldBe setOf(AVSLAG.visningsnavn)
+
+        //language=JSON
+        EmneknaggBuilder(
+            """
+                {
+                  "behandletHendelse": { "type": "Søknad" },
+                  "rettighetsperioder": [],
+                  "opplysninger": []
+                }
+            """,
+        ).bygg() shouldBe setOf(AVSLAG.visningsnavn)
+    }
+
+    @Test
+    fun `Emneknagger ved avslag av minsteinntekt og rettighet permittering`() {
+        val behandlingResultat =
+            lagBehandlingResultat(
+                behandletHendelseType = "Søknad",
+                harRettighet = false,
+                ForenkletOpplysning(
+                    id = OpplysningTyper.OPPFYLLER_KRAV_TIL_MINSTEINNTEKT.opplysningTypeId,
+                    verdi = false,
+                ),
+                ForenkletOpplysning(
+                    id = OpplysningTyper.RETTIGHET_DAGPEGNER_UNDER_PERMITTERING.opplysningTypeId,
+                    verdi = true,
+                ),
+            )
+
+        EmneknaggBuilder(json = behandlingResultat).bygg() shouldBe
+            setOf(
+                AVSLAG.visningsnavn,
+                AVSLAG_MINSTEINNTEKT.visningsnavn,
+                RETTIGHET_PERMITTERT.visningsnavn,
+            )
+    }
+
+    @Test
+    fun `emneknagger ved avslag pga alder og rettighet ordinær`() {
+        val behandlingResultat =
+            lagBehandlingResultat(
+                behandletHendelseType = "Søknad",
+                harRettighet = false,
+                ForenkletOpplysning(
+                    id = OpplysningTyper.KRAV_TIL_ALDER.opplysningTypeId,
+                    verdi = false,
+                ),
+                ForenkletOpplysning(
+                    id = OpplysningTyper.RETTIGHET_ORDINÆRE_DAGPENGER.opplysningTypeId,
+                    verdi = true,
+                ),
+            )
+
+        EmneknaggBuilder(json = behandlingResultat).bygg() shouldBe
+            setOf(
+                AVSLAG.visningsnavn,
+                AVSLAG_ALDER.visningsnavn,
+                RETTIGHET_ORDINÆR.visningsnavn,
+            )
+    }
+
+    @Test
+    fun `Emneknagger ved avslag og med flere avslagsgrunner`() {
+        val behandlingResultat =
+            lagBehandlingResultat(
+                behandletHendelseType = "Søknad",
+                harRettighet = false,
+                ForenkletOpplysning(
+                    id = OpplysningTyper.OPPFYLLER_KRAV_TIL_MINSTEINNTEKT.opplysningTypeId,
+                    verdi = false,
+                ),
+                ForenkletOpplysning(
+                    id = OpplysningTyper.KRAV_TIL_TAP_AV_ARBEIDSINNTEKT.opplysningTypeId,
+                    verdi = false,
+                ),
+                ForenkletOpplysning(
+                    id = OpplysningTyper.TAP_AV_ARBEIDSTID_ER_MINST_TERSKEL.opplysningTypeId,
+                    verdi = false,
+                ),
+                ForenkletOpplysning(
+                    id = OpplysningTyper.KRAV_TIL_ALDER.opplysningTypeId,
+                    verdi = false,
+                ),
+                ForenkletOpplysning(
+                    id = OpplysningTyper.IKKE_FULLE_YTELSER.opplysningTypeId,
+                    verdi = false,
+                ),
+                ForenkletOpplysning(
+                    id = OpplysningTyper.OPPFYLLER_MEDLEMSKAP.opplysningTypeId,
+                    verdi = false,
+                ),
+                ForenkletOpplysning(
+                    id = OpplysningTyper.IKKE_PÅVIRKET_AV_STREIK_ELLER_LOCKOUT.opplysningTypeId,
+                    verdi = false,
+                ),
+                ForenkletOpplysning(
+                    id = OpplysningTyper.OPPFYLLER_KRAVET_OPPHOLD.opplysningTypeId,
+                    verdi = false,
+                ),
+                ForenkletOpplysning(
+                    id = OpplysningTyper.KRAV_TIL_ARBEIDSSØKER.opplysningTypeId,
+                    verdi = false,
+                ),
+                ForenkletOpplysning(
+                    id = OpplysningTyper.OPPYLLER_KRAV_TIL_REGISTRERT_ARBEIDSSØKER.opplysningTypeId,
+                    verdi = false,
+                ),
+                ForenkletOpplysning(
+                    id = OpplysningTyper.OPPFYLLER_KRAV_TIL_IKKE_UTESTENGT.opplysningTypeId,
+                    verdi = false,
+                ),
+                ForenkletOpplysning(
+                    id = OpplysningTyper.KRAV_TIL_UTDANNING_ELLER_OPPLÆRING.opplysningTypeId,
+                    verdi = false,
+                ),
+            )
+        EmneknaggBuilder(json = behandlingResultat).bygg() shouldBe
+            setOf(
+                AVSLAG.visningsnavn,
+                AVSLAG_MINSTEINNTEKT.visningsnavn,
+                AVSLAG_ARBEIDSINNTEKT.visningsnavn,
+                AVSLAG_ARBEIDSTID.visningsnavn,
+                AVSLAG_ALDER.visningsnavn,
+                AVSLAG_ANDRE_YTELSER.visningsnavn,
+                AVSLAG_MEDLEMSKAP.visningsnavn,
+                AVSLAG_STREIK.visningsnavn,
+                AVSLAG_OPPHOLD_UTLAND.visningsnavn,
+                AVSLAG_REELL_ARBEIDSSØKER.visningsnavn,
+                AVSLAG_IKKE_REGISTRERT.visningsnavn,
+                AVSLAG_UTESTENGT.visningsnavn,
+                AVSLAG_UTDANNING.visningsnavn,
+            )
+    }
+
+    @Test
+    fun `emneknagger ved innvigelse av dagpenger etter verneplikt`() {
+        val behandlingResultat =
+            lagBehandlingResultat(
+                behandletHendelseType = "Søknad",
+                harRettighet = true,
+                ForenkletOpplysning(
+                    id = OpplysningTyper.RETTIGHET_DAGPENGER_ETTER_VERNEPLIKT.opplysningTypeId,
+                    verdi = true,
+                ),
+            )
+        EmneknaggBuilder(json = behandlingResultat).bygg() shouldBe
+            setOf(
+                INNVILGELSE.visningsnavn,
+                RETTIGHET_VERNEPLIKT.visningsnavn,
+            )
+    }
+
+    @Test
+    fun `emneknagger ved innvilgelse av ordinære dagpenger`() {
+        val behandlingResultat =
+            lagBehandlingResultat(
+                behandletHendelseType = "Søknad",
+                harRettighet = true,
+                ForenkletOpplysning(
+                    id = OpplysningTyper.RETTIGHET_ORDINÆRE_DAGPENGER.opplysningTypeId,
+                    verdi = true,
+                ),
+            )
+        EmneknaggBuilder(json = behandlingResultat).bygg() shouldBe
+            setOf(
+                INNVILGELSE.visningsnavn,
+                RETTIGHET_ORDINÆR.visningsnavn,
+            )
+    }
+
+    @Test
+    fun ` emneknagger ved innvilgelse av dagpenger under permittering`() {
+        val behanldingResultat =
+            lagBehandlingResultat(
+                behandletHendelseType = "Søknad",
+                harRettighet = true,
+                ForenkletOpplysning(
+                    id = OpplysningTyper.RETTIGHET_DAGPEGNER_UNDER_PERMITTERING.opplysningTypeId,
+                    verdi = true,
+                ),
+            )
+        EmneknaggBuilder(behanldingResultat).bygg() shouldBe
+            setOf(
+                INNVILGELSE.visningsnavn,
+                RETTIGHET_PERMITTERT.visningsnavn,
+            )
+    }
+
+    @Test
+    fun `emneknagger ved innvilgelse av dagpenger under permittering fra fiskeindustri`() {
+        val behandlingResultat =
+            lagBehandlingResultat(
+                behandletHendelseType = "Søknad",
+                harRettighet = true,
+                ForenkletOpplysning(
+                    id = OpplysningTyper.RETTIGHET_DAGPENGER_UNDER_PERMITTERING_I_FISKEFOREDLINGSINDUSTRI.opplysningTypeId,
+                    verdi = true,
+                ),
+            )
+        EmneknaggBuilder(behandlingResultat).bygg() shouldBe
+            setOf(
+                INNVILGELSE.visningsnavn,
+                Regelknagg.RETTIGHET_PERMITTERT_FISK.visningsnavn,
+            )
+    }
+
+    @Test
+    fun `emneknagger ved innvilgelse av dagpenger etter konkurs`() {
+        val behandlingResultat =
+            lagBehandlingResultat(
+                behandletHendelseType = "Søknad",
+                harRettighet = true,
+                ForenkletOpplysning(
+                    id = OpplysningTyper.RETTIGHET_DAGPENGER_ETTER_KONKURS.opplysningTypeId,
+                    verdi = true,
+                ),
+            )
+        EmneknaggBuilder(behandlingResultat).bygg() shouldBe
+            setOf(
+                INNVILGELSE.visningsnavn,
+                Regelknagg.RETTIGHET_KONKURS.visningsnavn,
+            )
+    }
+
+    @Test
+    fun `Skal kunne motta forslag_til_vedtak hendelse med innvilget ukjent rettighet - regelmotor har laget noe vi ikke kjenner`() {
+        val behandlingResultat =
+            lagBehandlingResultat(
+                behandletHendelseType = "Søknad",
+                harRettighet = true,
+                ForenkletOpplysning(
+                    id = UUID.randomUUID(),
+                    verdi = true,
+                ),
+            )
+        EmneknaggBuilder(behandlingResultat).bygg() shouldBe setOf(INNVILGELSE.visningsnavn)
+    }
+
+    @Test
+    fun `emneknagger for behandling resultat med behandlingtype meldekort`() {
+        val behandlingResultat =
+            lagBehandlingResultat(
+                behandletHendelseType = "Meldekort",
+                harRettighet = false,
+                ForenkletOpplysning(
+                    id = UUID.randomUUID(),
+                    verdi = true,
+                ),
+            )
+
+        EmneknaggBuilder(behandlingResultat).bygg() shouldBe
+            setOf(
+                AVSLAG.visningsnavn, MELDEKORT.visningsnavn,
+            )
+    }
+
+    @Test
+    fun `emneknagger for behandling resultat med behandlingtype manuell`() {
+        val behandlingResultat =
+            lagBehandlingResultat(
+                behandletHendelseType = "Manuell",
+                harRettighet = false,
+                ForenkletOpplysning(
+                    id = UUID.randomUUID(),
+                    verdi = true,
+                ),
+            )
+
+        EmneknaggBuilder(behandlingResultat).bygg() shouldBe
+            setOf(
+                AVSLAG.visningsnavn, BehandletHendelseType.MANUELL.visningsnavn,
+            )
+    }
+
+    private val objectMapper =
+        jacksonObjectMapper().also {
+            it.registerModule(JavaTimeModule())
+        }
+
+    private data class ForenkletOpplysning(val id: UUID, val verdi: Boolean)
+
+    private fun lagBehandlingResultat(
+        behandletHendelseType: String = "Søknad",
+        harRettighet: Boolean,
+        vararg opplysninger: ForenkletOpplysning,
+    ): String {
+        val behandletHendelseObject =
+            objectMapper.createObjectNode().apply {
+                put("id", UUID.randomUUID().toString())
+                put("type", behandletHendelseType)
+            }
+
+        val rettighetPeriodeObject =
+            objectMapper.createObjectNode().apply {
+                put("harRett", harRettighet)
+            }
+        val rettighetsPerioderArray =
+            objectMapper.createArrayNode().apply {
+                add(rettighetPeriodeObject)
+            }
+
+        val opplysningArray =
+            objectMapper.createArrayNode().apply {
+                opplysninger.forEach { opplysning ->
+                    val verdiNode =
+                        objectMapper.createObjectNode().apply {
+                            put("verdi", opplysning.verdi)
+                            put("datatype", "boolsk")
+                        }
+                    val periodeObject =
+                        objectMapper.createObjectNode().apply {
+                            set<ObjectNode>("verdi", verdiNode)
+                        }
+                    val periodeArray =
+                        objectMapper.createArrayNode().apply {
+                            add(periodeObject)
+                        }
+                    val opplysningObject =
+                        objectMapper.createObjectNode().apply {
+                            put("opplysningTypeId", opplysning.id.toString())
+                            set<ArrayNode>("perioder", periodeArray)
+                        }
+                    add(opplysningObject)
+                }
+            }
+        return objectMapper.createObjectNode().apply {
+            put("behandletHendelse", behandletHendelseObject)
+            put("rettighetsperioder", rettighetsPerioderArray)
+            put("opplysninger", opplysningArray)
+        }.toString()
+    }
+}
