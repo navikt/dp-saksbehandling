2c21c7493316845f217fc7561e8a58a8f8b04d9f
Giao The Cung <giaothe@gmail.com>
2025-09-09 10:55:42 +0200

    Mottak for behandling resultat forslag

---
+++ mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/mottak/ForslagTilBehandlingResultatMottakTest.kt
@@ -0,0 +1,71 @@
+package no.nav.dagpenger.saksbehandling.mottak
+
+import com.github.navikt.tbd_libs.rapids_and_rivers.test_support.TestRapid
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.dagpenger.saksbehandling.Emneknagg.BehandletHendelseType.MANUELL
+import no.nav.dagpenger.saksbehandling.Emneknagg.BehandletHendelseType.MELDEKORT
+import no.nav.dagpenger.saksbehandling.Emneknagg.Regelknagg.AVSLAG
+import no.nav.dagpenger.saksbehandling.OppgaveMediator
+import no.nav.dagpenger.saksbehandling.UUIDv7
+import no.nav.dagpenger.saksbehandling.hendelser.ForslagTilVedtakHendelse
+import org.junit.jupiter.api.Test
+import kotlin.also
+
+class ForslagTilBehandlingResultatMottakTest {
+    private val testRapid = TestRapid()
+    private val behandlingId = UUIDv7.ny()
+    private val søknadId = UUIDv7.ny()
+    private val ident = "123456678912"
+
+    @Test
+    fun `Skal håndtere relevante pakker`() {
+        val slots = mutableListOf<ForslagTilVedtakHendelse>()
+        val oppgaveMediator =
+            mockk<OppgaveMediator>().also {
+                every { it.opprettEllerOppdaterOppgave(capture(slots)) }
+            }
+        ForslagTilBehandlingResultatMottak(testRapid, oppgaveMediator)
+
+        listOf("Søknad", "Meldekort", "Manuell").forEachIndexed { index, behandletHendelseType ->
+            testRapid.sendTestMessage(testMessage(behandletHendelseType = behandletHendelseType))
+            slots[index].let { hendelse ->
+                hendelse.ident shouldBe ident
+                hendelse.behandletHendelseId shouldBe søknadId.toString()
+                hendelse.behandlingId shouldBe behandlingId
+                hendelse.behandletHendelseType shouldBe behandletHendelseType
+            }
+        }
+
+        slots.single { it.behandletHendelseType == "Søknad" }.emneknagger shouldBe setOf(AVSLAG.visningsnavn)
+
+        slots.single { it.behandletHendelseType == "Meldekort" }.emneknagger shouldBe
+            setOf(
+                AVSLAG.visningsnavn,
+                MELDEKORT.visningsnavn,
+            )
+        slots.single { it.behandletHendelseType == "Manuell" }.emneknagger shouldBe
+            setOf(
+                AVSLAG.visningsnavn,
+                MANUELL.visningsnavn,
+            )
+    }
+
+    private fun testMessage(behandletHendelseType: String): String {
+        //language=JSON
+        return """
+            {
+              "@event_name": "forslag_til_behandlingsresultat",
+              "ident": "$ident",
+              "behandlingId": "$behandlingId",
+              "behandletHendelse": {
+                "id": "$søknadId",
+                "type": "$behandletHendelseType"
+              },
+              "opplysninger": [],
+              "rettighetsperioder": []
+            }
+            """.trimIndent()
+    }
+}
