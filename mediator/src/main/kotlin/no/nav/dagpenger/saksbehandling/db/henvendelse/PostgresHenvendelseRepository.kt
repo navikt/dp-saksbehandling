570e30ba11dd3642d60b1e9da9589b01eafe91c2
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2025-11-03 14:37:34 +0100

    Lagrer, endrer og henter opp igjen henvendelse fra basen
    Co-authored-by: Giao The Cung <giaothe@gmail.com>

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/db/henvendelse/PostgresHenvendelseRepository.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/db/henvendelse/PostgresHenvendelseRepository.kt
@@ -9,9 +9,11 @@ import no.nav.dagpenger.saksbehandling.AdressebeskyttelseGradering
 import no.nav.dagpenger.saksbehandling.Person
 import no.nav.dagpenger.saksbehandling.Tilstandsendring
 import no.nav.dagpenger.saksbehandling.db.oppgave.DataNotFoundException
+import no.nav.dagpenger.saksbehandling.hendelser.FjernAnsvarHendelse
 import no.nav.dagpenger.saksbehandling.hendelser.Hendelse
 import no.nav.dagpenger.saksbehandling.hendelser.HenvendelseMottattHendelse
 import no.nav.dagpenger.saksbehandling.hendelser.Kategori
+import no.nav.dagpenger.saksbehandling.hendelser.OpprettManuellBehandlingHendelse
 import no.nav.dagpenger.saksbehandling.hendelser.TildelHendelse
 import no.nav.dagpenger.saksbehandling.henvendelse.Henvendelse
 import no.nav.dagpenger.saksbehandling.henvendelse.Henvendelse.Tilstand.Type
@@ -172,6 +174,8 @@ class PostgresHenvendelseRepository(private val dataSource: DataSource) : Henven
         return when (hendelseType) {
             "HenvendelseMottattHendelse" -> hendelseJson.tilHendelse<HenvendelseMottattHendelse>()
             "TildelHendelse" -> hendelseJson.tilHendelse<TildelHendelse>()
+            "FjernAnsvarHendelse" -> hendelseJson.tilHendelse<FjernAnsvarHendelse>()
+            "OpprettManuellBehandlingHendelse" -> hendelseJson.tilHendelse<OpprettManuellBehandlingHendelse>()
             else -> {
                 logger.error { "rehydrerTilstandsendringHendelse: Ukjent hendelse med type $hendelseType" }
                 sikkerlogger.error { "rehydrerTilstandsendringHendelse: Ukjent hendelse med type $hendelseType: $hendelseJson" }
--- mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/db/henvendelse/PostgresHenvendelseRepositoryTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/db/henvendelse/PostgresHenvendelseRepositoryTest.kt
@@ -2,10 +2,13 @@ package no.nav.dagpenger.saksbehandling.db.henvendelse
 
 import io.kotest.matchers.shouldBe
 import no.nav.dagpenger.saksbehandling.Saksbehandler
+import no.nav.dagpenger.saksbehandling.UUIDv7
 import no.nav.dagpenger.saksbehandling.db.DBTestHelper
 import no.nav.dagpenger.saksbehandling.db.DBTestHelper.Companion.testPerson
+import no.nav.dagpenger.saksbehandling.hendelser.FjernAnsvarHendelse
 import no.nav.dagpenger.saksbehandling.hendelser.HenvendelseMottattHendelse
 import no.nav.dagpenger.saksbehandling.hendelser.Kategori
+import no.nav.dagpenger.saksbehandling.hendelser.OpprettManuellBehandlingHendelse
 import no.nav.dagpenger.saksbehandling.hendelser.TildelHendelse
 import no.nav.dagpenger.saksbehandling.henvendelse.Henvendelse
 import org.junit.jupiter.api.Test
@@ -14,14 +17,21 @@ import java.time.temporal.ChronoUnit
 
 class PostgresHenvendelseRepositoryTest {
     @Test
-    fun `Skal lagres og hente henvendelse fra database`() {
+    fun `Skal lagre endre og hente henvendelse fra database`() {
         DBTestHelper.withPerson { ds ->
+            val nå = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)
+            val saksbehandler =
+                Saksbehandler(
+                    navIdent = "Z12345",
+                    grupper = emptySet(),
+                    tilganger = emptySet(),
+                )
             val repository = PostgresHenvendelseRepository(ds)
             val henvendelseMottattHendelse =
                 HenvendelseMottattHendelse(
                     ident = testPerson.ident,
                     journalpostId = "jp12",
-                    registrertTidspunkt = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS),
+                    registrertTidspunkt = nå,
                     søknadId = null,
                     skjemaKode = "skjemaKode",
                     kategori = Kategori.GENERELL,
@@ -39,13 +49,44 @@ class PostgresHenvendelseRepositoryTest {
             henvendelse.tildel(
                 tildelHendelse =
                     TildelHendelse(
-                        utførtAv =
-                            Saksbehandler(
-                                navIdent = "Z12345",
-                                grupper = emptySet(),
-                                tilganger = emptySet(),
-                            ),
+                        utførtAv = saksbehandler,
                         ansvarligIdent = "ABS123",
+                    ),
+            )
+            repository.lagre(henvendelse)
+
+            repository.hent(henvendelse.henvendelseId) shouldBe henvendelse
+
+            henvendelse.leggTilbake(
+                fjernAnsvarHendelse =
+                    FjernAnsvarHendelse(
+                        utførtAv = saksbehandler,
+                    ),
+            )
+            repository.lagre(henvendelse)
+
+            repository.hent(henvendelse.henvendelseId) shouldBe henvendelse
+
+            henvendelse.tildel(
+                tildelHendelse =
+                    TildelHendelse(
+                        utførtAv = saksbehandler,
+                        ansvarligIdent = "ABS123",
+                    ),
+            )
+            repository.lagre(henvendelse)
+
+            repository.hent(henvendelse.henvendelseId) shouldBe henvendelse
+
+            henvendelse.ferdigstill(
+                opprettManuellBehandlingHendelse =
+                    OpprettManuellBehandlingHendelse(
+                        manuellId = UUIDv7.ny(),
+                        behandlingId = UUIDv7.ny(),
+                        ident = testPerson.ident,
+                        opprettet = nå,
+                        basertPåBehandling = UUIDv7.ny(),
+                        utførtAv = saksbehandler,
                     ),
             )
             repository.lagre(henvendelse)
