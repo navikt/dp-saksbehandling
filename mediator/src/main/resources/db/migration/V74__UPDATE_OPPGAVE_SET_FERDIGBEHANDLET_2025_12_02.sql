4170e47f492a5aa06ed57f9bf5f7ae4a0a09c7e3
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2025-12-02 10:18:36 +0100

    Rettet feil i skript
    Co-authored-by: Giao The Cung <giaothe@gmail.com>

--- mediator/src/main/resources/db/migration/V74__UPDATE_OPPGAVE_SET_FERDIGBEHANDLET_2025_12_02.sql
+++ mediator/src/main/resources/db/migration/V74__UPDATE_OPPGAVE_SET_FERDIGBEHANDLET_2025_12_02.sql
@@ -1,20 +1,36 @@
-UPDATE oppgave_v1
-SET    tilstand = 'FERDIG_BEHANDLET'
-WHERE  id = '019ad567-0e16-77c9-bea7-38446159c61a'
-AND    tilstand = 'UNDER_BEHANDLING';
+-- Pga. timeout mot regelmotor, oppstod inkonsistens, slik at oppgave ble stående i tilstand UNDER_BEHANDLING etter at vedtak var fattet.
+-- Vi oppdaterer manuelt til FERDIG_BEHANDLET her.
+DO $$
+    DECLARE
+        oppgave RECORD;
+    BEGIN
+        FOR oppgave IN
+            SELECT  *
+            FROM    oppgave_v1 oppg
+            WHERE   oppg.tilstand = 'UNDER_BEHANDLING'
+            AND     oppg.id = '019ad567-0e16-77c9-bea7-38446159c61a'
+            LOOP
+                -- Oppdater oppgave_v1
+                UPDATE  oppgave_v1 oppg
+                SET     tilstand = 'FERDIG_BEHANDLET'
+                WHERE   oppg.id = oppgave.id;
 
-INSERT INTO oppgave_tilstand_logg_v1 (
-    id,
-    oppgave_id,
-    tilstand,
-    hendelse_type,
-    hendelse,
-    tidspunkt
-) VALUES (
-    gen_random_uuid()
-    , '019ad567-0e16-77c9-bea7-38446159c61a'
-    , 'FERDIG_BEHANDLET'
-    , 'GodkjentBehandlingHendelse'
-    , '{"oppgaveId": "019ad567-0e16-77c9-bea7-38446159c61a", "utførtAv": {"grupper": ["2e9c63d8-322e-4c1f-b500-a0abb812761c"], "navIdent": "A164486", "tilganger": ["SAKSBEHANDLER"]}, "meldingOmVedtakKilde": "DP_SAK"}'
-    , now() + INTERVAL '1 hours'
-);
+                -- Sett inn logg i oppgave_tilstand_logg_v1
+                INSERT INTO oppgave_tilstand_logg_v1
+                ( id
+                , oppgave_id
+                , tilstand
+                , hendelse_type
+                , hendelse
+                , tidspunkt
+                )
+                VALUES
+                    ( gen_random_uuid()
+                    , oppgave.id
+                    , 'FERDIG_BEHANDLET'
+                    , 'GodkjentBehandlingHendelse'
+                    , '{"oppgaveId": "019ad567-0e16-77c9-bea7-38446159c61a", "utførtAv": {"grupper": ["2e9c63d8-322e-4c1f-b500-a0abb812761c"], "navIdent": "A164486", "tilganger": ["SAKSBEHANDLER"]}, "meldingOmVedtakKilde": "DP_SAK"}'
+                    , now() + INTERVAL '1 hours'
+                    );
+            END LOOP;
+    END $$;
