87e58ba6b38c2c065dffc779bf7588e3c8552b7f
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2026-02-26 11:22:03 +0100

    UtsendingMediator test for automatiske vedtak
    Co-authored-by: Giao The Cung <giaothe@gmail.com>

--- mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/utsending/UtsendingMediatorTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/saksbehandling/utsending/UtsendingMediatorTest.kt
@@ -426,16 +426,13 @@ class UtsendingMediatorTest {
                 rapidsConnection = rapid,
             )
 
-            utsendingMediator.startUtsendingForAutomatiskVedtakFattet(
-                vedtakFattetHendelse =
-                    VedtakFattetHendelse(
-                        behandlingId = behandlingId,
-                        behandletHendelseId = UUIDv7.ny().toString(),
-                        behandletHendelseType = "Søknad",
-                        ident = person.ident,
-                        sak = utsendingSak,
-                        automatiskBehandlet = true,
-                    ),
+            rapid.sendTestMessage(
+                behandlingsresultatEvent(
+                    ident = person.ident,
+                    behandlingId = behandling.behandlingId.toString(),
+                    behandletHendelseId = søknadId.toString(),
+                    automatiskBehandling = true,
+                ),
             )
 
             var utsending = utsendingRepository.hentUtsendingForBehandlingId(behandlingId)
@@ -443,113 +440,91 @@ class UtsendingMediatorTest {
             utsending.tilstand().type shouldBe AvventerArkiverbarVersjonAvBrev
             utsending.sak() shouldBe utsendingSak
             utsending.brev() shouldBe htmlBrev
-// ///////////////////////
-//            val message =
-//                behandlingsresultatEvent(
-//                    ident = person.ident,
-//                    behandlingId = behandling.behandlingId.toString(),
-//                    behandletHendelseId = søknadId.toString(),
-//                    behandletHendelseType = "Søknad",
-//                    harRett = true,
-//                )
-//
-//            rapid.sendTestMessage(message = message)
-//
-//            utsendingRepository.finnUtsendingForBehandlingId(behandlingId).let { utsending ->
-//                requireNotNull(utsending)
-//                utsending.brev() shouldBe htmlBrev
-//            }
-//
-//            utsending = utsendingRepository.hentUtsendingForBehandlingId(behandlingId)
-//
-//            utsending.sak() shouldBe utsendingSak
-//            utsending.tilstand().type shouldBe AvventerArkiverbarVersjonAvBrev
-// ///////////////////////
 
-//            rapid.inspektør.size shouldBe 1
-//            val htmlBrevAsBase64 = Base64.getEncoder().encode(htmlBrev.toByteArray()).toString(Charsets.UTF_8)
-//            rapid.inspektør.message(0).toString() shouldEqualSpecifiedJson
-//                    //language=JSON
-//                    """
-//                {
-//                   "@event_name": "behov",
-//                   "@behov": [
-//                     "${ArkiverbartBrevBehov.BEHOV_NAVN}"
-//                   ],
-//                   "htmlBase64": "$htmlBrevAsBase64",
-//                   "dokumentNavn": "vedtak.pdf",
-//                   "kontekst": "behandling/$behandlingId",
-//                   "ident": "${person.ident}",
-//                   "sak": {
-//                      "id": "${utsendingSak.id}",
-//                      "kontekst": "${utsendingSak.kontekst}"
-//                  }
-//                }
-//                """.trimIndent()
-//
-//            val pdfUrnString = "urn:pdf:123"
-//            rapid.sendTestMessage(
-//                arkiverbartDokumentBehovLøsning(
-//                    behandlingId = behandlingId,
-//                    pdfUrnString = pdfUrnString,
-//                ),
-//            )
-//
-//            utsending = utsendingRepository.hentUtsendingForBehandlingId(behandlingId)
-//            utsending.tilstand().type shouldBe AvventerJournalføring
-//            utsending.pdfUrn() shouldBe pdfUrnString.toUrn()
-//            rapid.inspektør.size shouldBe 2
-//            rapid.inspektør.message(1).toString() shouldEqualSpecifiedJson
-//                    //language=JSON
-//                    """
-//                {
-//                  "@event_name": "behov",
-//                  "@behov": [
-//                    "${JournalføringBehov.BEHOV_NAVN}"
-//                  ],
-//                  "tittel" : "${UtsendingType.KLAGEMELDING.brevTittel}",
-//                  "skjemaKode" : "${UtsendingType.KLAGEMELDING.skjemaKode}",
-//                  "ident": "${person.ident}",
-//                  "pdfUrn": "$pdfUrnString",
-//                  "sak": {
-//                    "id": "${utsendingSak.id}",
-//                    "kontekst": "${utsendingSak.kontekst}"
-//                  }
-//                }
-//                """.trimIndent()
-//
-//            val journalpostId = "123"
-//            rapid.sendTestMessage(journalføringBehovLøsning(behandlingId = behandlingId, journalpostId = journalpostId))
-//
-//            utsending = utsendingRepository.hentUtsendingForBehandlingId(behandlingId)
-//            utsending.tilstand().type shouldBe AvventerDistribuering
-//            utsending.journalpostId() shouldBe journalpostId
-//            utsending.sak() shouldBe utsendingSak
-//            rapid.inspektør.size shouldBe 3
-//            rapid.inspektør.message(2).toString() shouldEqualSpecifiedJson
-//                    //language=JSON
-//                    """
-//                {
-//                  "@event_name": "behov",
-//                  "@behov": [
-//                    "${DistribueringBehov.BEHOV_NAVN}"
-//                  ],
-//                  "journalpostId": "${utsending.journalpostId()}",
-//                  "fagsystem": "${utsendingSak.kontekst}"
-//                }
-//                """.trimIndent()
-//
-//            val distribusjonId = "distribusjonId"
-//            rapid.sendTestMessage(
-//                distribuertDokumentBehovLøsning(
-//                    behandlingId = behandlingId,
-//                    journalpostId = journalpostId,
-//                    distribusjonId = distribusjonId,
-//                ),
-//            )
-//            utsending = utsendingRepository.hentUtsendingForBehandlingId(behandlingId)
-//            utsending.tilstand().type shouldBe Distribuert
-//            utsending.distribusjonId() shouldBe distribusjonId
+            rapid.inspektør.size shouldBe 1
+            val htmlBrevAsBase64 = Base64.getEncoder().encode(htmlBrev.toByteArray()).toString(Charsets.UTF_8)
+            rapid.inspektør.message(0).toString() shouldEqualSpecifiedJson
+                //language=JSON
+                """
+                {
+                   "@event_name": "behov",
+                   "@behov": [
+                     "${ArkiverbartBrevBehov.BEHOV_NAVN}"
+                   ],
+                   "htmlBase64": "$htmlBrevAsBase64",
+                   "dokumentNavn": "vedtak.pdf",
+                   "kontekst": "behandling/$behandlingId",
+                   "ident": "${person.ident}",
+                   "sak": {
+                      "id": "${utsendingSak.id}",
+                      "kontekst": "${utsendingSak.kontekst}"
+                  }
+                }
+                """.trimIndent()
+
+            val pdfUrnString = "urn:pdf:123"
+            rapid.sendTestMessage(
+                arkiverbartDokumentBehovLøsning(
+                    behandlingId = behandlingId,
+                    pdfUrnString = pdfUrnString,
+                ),
+            )
+
+            utsending = utsendingRepository.hentUtsendingForBehandlingId(behandlingId)
+            utsending.tilstand().type shouldBe AvventerJournalføring
+            utsending.pdfUrn() shouldBe pdfUrnString.toUrn()
+            rapid.inspektør.size shouldBe 2
+            rapid.inspektør.message(1).toString() shouldEqualSpecifiedJson
+                //language=JSON
+                """
+                {
+                  "@event_name": "behov",
+                  "@behov": [
+                    "${JournalføringBehov.BEHOV_NAVN}"
+                  ],
+                  "tittel" : "${UtsendingType.VEDTAK_DAGPENGER.brevTittel}",
+                  "skjemaKode" : "${UtsendingType.VEDTAK_DAGPENGER.skjemaKode}",
+                  "ident": "${person.ident}",
+                  "pdfUrn": "$pdfUrnString",
+                  "sak": {
+                    "id": "${utsendingSak.id}",
+                    "kontekst": "${utsendingSak.kontekst}"
+                  }
+                }
+                """.trimIndent()
+
+            val journalpostId = "123"
+            rapid.sendTestMessage(journalføringBehovLøsning(behandlingId = behandlingId, journalpostId = journalpostId))
+
+            utsending = utsendingRepository.hentUtsendingForBehandlingId(behandlingId)
+            utsending.tilstand().type shouldBe AvventerDistribuering
+            utsending.journalpostId() shouldBe journalpostId
+            utsending.sak() shouldBe utsendingSak
+            rapid.inspektør.size shouldBe 3
+            rapid.inspektør.message(2).toString() shouldEqualSpecifiedJson
+                //language=JSON
+                """
+                {
+                  "@event_name": "behov",
+                  "@behov": [
+                    "${DistribueringBehov.BEHOV_NAVN}"
+                  ],
+                  "journalpostId": "${utsending.journalpostId()}",
+                  "fagsystem": "${utsendingSak.kontekst}"
+                }
+                """.trimIndent()
+
+            val distribusjonId = "distribusjonId"
+            rapid.sendTestMessage(
+                distribuertDokumentBehovLøsning(
+                    behandlingId = behandlingId,
+                    journalpostId = journalpostId,
+                    distribusjonId = distribusjonId,
+                ),
+            )
+            utsending = utsendingRepository.hentUtsendingForBehandlingId(behandlingId)
+            utsending.tilstand().type shouldBe Distribuert
+            utsending.distribusjonId() shouldBe distribusjonId
         }
     }
 
