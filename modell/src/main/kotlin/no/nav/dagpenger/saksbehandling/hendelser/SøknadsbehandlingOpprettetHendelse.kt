52c9f0f83a0cbc19ddff2730fd9f938736eddcd1
Giao The Cung <giaothe@gmail.com>
2026-04-30 14:15:35 +0200

    Fjerne død kode

--- modell/src/main/kotlin/no/nav/dagpenger/saksbehandling/hendelser/SøknadsbehandlingOpprettetHendelse.kt
+++ modell/src/main/kotlin/no/nav/dagpenger/saksbehandling/hendelser/SøknadsbehandlingOpprettetHendelse.kt
@@ -1,10 +1,5 @@
 package no.nav.dagpenger.saksbehandling.hendelser
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.dagpenger.saksbehandling.Applikasjon
 import java.time.LocalDateTime
 import java.util.UUID
@@ -17,13 +12,4 @@ data class SøknadsbehandlingOpprettetHendelse(
     val basertPåBehandling: UUID? = null,
     val behandlingskjedeId: UUID? = null,
     override val utførtAv: Applikasjon = Applikasjon.DpBehandling,
-) : Hendelse(utførtAv) {
-    companion object {
-        private val objectMapper: ObjectMapper =
-            jacksonObjectMapper()
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-                .enable(SerializationFeature.INDENT_OUTPUT)
-    }
-}
+) : Hendelse(utførtAv)
