4c178176e62c4520a94bb235266d6bfb46b6056a
Giao The Cung <giaothe@gmail.com>
2026-06-01 16:17:35 +0200

    skip

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/klage/UtsendingDistribuertMottakForKlage.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/klage/UtsendingDistribuertMottakForKlage.kt
@@ -53,6 +53,7 @@ internal class UtsendingDistribuertMottakForKlage(
             setOf(
                 "019938f9-23cf-7abe-b3de-797c59c5df0f",
                 "019e8347-7ce1-757d-a9c1-c03f3d950ee8",
+                "019e8388-fb91-73d7-998e-d0b7f0b04fc1",
             )
         ) {
             logger.warn { "Skipper behandlingId: $behandlingId fra UtsendingDistribuertMottakForKlage" }
