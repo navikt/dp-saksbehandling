92c859b16fda6eaddc5fdff19fb478d5b1af0974
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2026-09-01 10:42:00 +0200

    Skip behandling i dev. Fix ktlint.
    Co-authored-by: Giao The Cung <giaothe@gmail.com>

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/utsending/mottak/BehandlingsresultatMottakForAutomatiskVedtakUtsending.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/utsending/mottak/BehandlingsresultatMottakForAutomatiskVedtakUtsending.kt
@@ -6,12 +6,12 @@ import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageMetadata
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.micrometer.core.instrument.MeterRegistry
-import java.util.UUID
 import no.nav.dagpenger.saksbehandling.UtsendingSak
 import no.nav.dagpenger.saksbehandling.db.sak.SakRepository
 import no.nav.dagpenger.saksbehandling.mottak.AbstractBehandlingsresultatMottak
 import no.nav.dagpenger.saksbehandling.mottak.Behandlingsresultat
 import no.nav.dagpenger.saksbehandling.utsending.UtsendingMediator
+import java.util.UUID
 
 private val logger = KotlinLogging.logger {}
 
@@ -37,8 +37,10 @@ internal class BehandlingsresultatMottakForAutomatiskVedtakUtsending(
     ) {
         val skipSetBehandlingId = setOf<UUID>(UUID.fromString("01a05bcc-9ee5-7175-9d57-ef52e15ffa9b"))
         if (behandlingsresultat.behandlingId in skipSetBehandlingId) {
-            logger.info { "Skipper behandlingId: ${behandlingsresultat.behandlingId} fra " +
-                    "BehandlingsresultatMottakForAutomatiskVedtakUtsending" }
+            logger.info {
+                "Skipper behandlingId: ${behandlingsresultat.behandlingId} fra " +
+                    "BehandlingsresultatMottakForAutomatiskVedtakUtsending"
+            }
             return
         }
         val sakId = sakRepository.hentSakIdForBehandlingId(behandlingsresultat.behandlingId).toString()
