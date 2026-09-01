9f2a0ef84fb2567f0811c9a3fc04b0ac4d7ca2bd
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2026-09-01 10:39:12 +0200

    Skip behandling i dev
    Co-authored-by: Giao The Cung <giaothe@gmail.com>

--- mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/utsending/mottak/BehandlingsresultatMottakForAutomatiskVedtakUtsending.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/saksbehandling/utsending/mottak/BehandlingsresultatMottakForAutomatiskVedtakUtsending.kt
@@ -4,12 +4,16 @@ import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageMetadata
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.micrometer.core.instrument.MeterRegistry
+import java.util.UUID
 import no.nav.dagpenger.saksbehandling.UtsendingSak
 import no.nav.dagpenger.saksbehandling.db.sak.SakRepository
 import no.nav.dagpenger.saksbehandling.mottak.AbstractBehandlingsresultatMottak
 import no.nav.dagpenger.saksbehandling.mottak.Behandlingsresultat
 import no.nav.dagpenger.saksbehandling.utsending.UtsendingMediator
+
+private val logger = KotlinLogging.logger {}
 
 internal class BehandlingsresultatMottakForAutomatiskVedtakUtsending(
     rapidsConnection: RapidsConnection,
@@ -31,6 +35,11 @@ internal class BehandlingsresultatMottakForAutomatiskVedtakUtsending(
         metadata: MessageMetadata,
         meterRegistry: MeterRegistry,
     ) {
+        val skipSetBehandlingId = setOf<UUID>(UUID.fromString("01a05bcc-9ee5-7175-9d57-ef52e15ffa9b"))
+        if (behandlingsresultat.behandlingId in skipSetBehandlingId) {
+            logger.info { "Skipper behandlingId: ${behandlingsresultat.behandlingId} fra BehandlingsresultatMottakForAutomatiskVedtakUtsending" }
+            return
+        }
         val sakId = sakRepository.hentSakIdForBehandlingId(behandlingsresultat.behandlingId).toString()
         val vedtakFattetHendelse =
             packet.vedtakFattetHendelse(
